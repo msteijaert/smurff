3d123e6381b487a2a02747b80bf398832132f75e
Tom Vander Aa <vanderaa@imec.be>
2016-12-09 11:45:31 +0100

    gen_data fixes

--- python/gen_data/generate.py
+++ python/gen_data/generate.py
@@ -3,14 +3,16 @@ import numpy as np
 
 # product of two gaussian low-rank matrices + noise
 def normal_dense(N, D, K):
-    X = np.random.normal(N,K)
-    W = np.random.normal(D,K)
-    return np.cross(X, W) + np.random.normal(N,D)
+    X = np.random.normal((N,K))
+    W = np.random.normal((D,K))
+    return np.dot(X, W.transpose()) + np.random.normal(N,D)
 
 # product of two low-rank 'ones' matrices
 def ones_dense(N, D, K):
-    X = np.ones(N,K)
-    W = np.ones(D,K)
-    return np.cross(X, W)
+    X = np.ones((N,K))
+    W = np.ones((D,K))
+    return np.dot(X,W.transpose())
 
-
+if __name__ == "__main__":
+    normal_dense(20,10,4)
+    ones_dense(20,10,4)
