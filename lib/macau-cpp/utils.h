2ac6f6513e9d28cdff065b27ef2e0cca0c7ab540
Tom Vander Aa <vanderaa@imec.be>
2017-01-24 16:47:00 +0100

    Cleanup utils

--- lib/macau-cpp/utils.h
+++ lib/macau-cpp/utils.h
@@ -14,7 +14,11 @@ extern "C" {
   #include <csr.h>
 }
 
+#ifdef NDEBUG
+#define SHOW(m)
+#else
 #define SHOW(m) std::cout << #m << ": " << m << std::endl;
+#endif
 
 class SparseFeat {
   public:
@@ -202,4 +206,29 @@ inline Eigen::MatrixXd to_coo(const Eigen::SparseMatrix<double> &Y) {
     return coords;
 }
 
+#if defined(_OPENMP)
+#include <omp.h>
 
+inline int thread_num() { return omp_get_thread_num(); }
+inline int nthreads() 
+{
+    int nt = -1;
+#pragma omp parallel 
+   {
+#pragma omp single
+      {
+         nt = omp_get_num_threads();
+      }
+   }
+
+   return nt;
+}
+#else
+inline int thread_num() { return 0; }
+inline int nthreads(void) { return 1; }
+#endif
+
+#pragma omp declare reduction (VectorPlus : VectorNd : omp_out += omp_in) initializer(omp_priv = VectorNd::Zero())
+#pragma omp declare reduction (MatrixPlus : MatrixNNd : omp_out += omp_in) initializer(omp_priv = MatrixNNd::Zero())
+
+#endif
