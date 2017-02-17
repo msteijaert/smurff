cf11e207a4598ed27209f51e064bd91ef309cd24
Tom Vander Aa <vanderaa@imec.be>
2017-02-17 12:12:28 +0100

    bench outer product

--- lib/macau-cpp/model.cpp
+++ lib/macau-cpp/model.cpp
@@ -101,7 +101,7 @@ void Factors::update_predictions(int iter, int burnin)
 
     double se = 0.0, se_avg = 0.0;
     const double inorm = 1.0 / (iter - burnin - 1);
-#pragma omp parallel for schedule(dynamic,8) reduction(+:se, se_avg)
+#pragma omp parallel for schedule(guided) reduction(+:se, se_avg)
     for (int k = 0; k < Ytest.outerSize(); ++k) {
         int idx = Ytest.outerIndexPtr()[k];
         for (Eigen::SparseMatrix<double>::InnerIterator it(Ytest,k); it; ++it) {
@@ -479,3 +479,56 @@ template struct DenseMF<Eigen::MatrixXd>;
 template struct DenseMF<SparseMatrixD>;
 
 } //end namespace Macau
+
+#ifdef BENCH
+
+#include "utils.h"
+
+int main()
+{
+    const int N = 32 * 1024;
+    const int K = 96;
+    const int R = 20;
+    {
+        init_bmrng(1234);
+        MatrixXd U(K,N);
+        bmrandn(U);
+
+        MatrixXd M(K,K) ;
+        double start = tick();
+        for(int i=0; i<R; ++i) {
+            M.setZero();
+            for(int j=0; j<N;++j) {
+                const auto &col = U.col(j);
+                M.noalias() += col * col.transpose();
+            }
+        }
+        double stop = tick();
+        std::cout << "norm U: " << U.norm() << std::endl;
+        std::cout << "norm M: " << M.norm() << std::endl;
+        std::cout << "MatrixXd: " << stop - start << std::endl;
+    }
+
+    {
+        init_bmrng(1234);
+        Matrix<double, K, Dynamic> U(K,N);
+        U = nrandn(K,N);
+
+        Matrix<double,K,K> M;
+        double start = tick();
+        for(int i=0; i<R; ++i) {
+            M.setZero();
+            for(int j=0; j<N;++j) {
+                const auto &col = U.col(j);
+                M.noalias() += col * col.transpose();
+            }
+        }
+        double stop = tick();
+        std::cout << "norm U: " << U.norm() << std::endl;
+        std::cout << "norm M: " << M.norm() << std::endl;
+        std::cout << "MatrixNNd: " << stop - start << std::endl;
+    }
+
+    return 0;
+}
+#endif
