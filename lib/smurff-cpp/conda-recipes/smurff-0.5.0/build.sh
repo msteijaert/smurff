806ba181f70464d4009a2579fccd1520890adce4
Tom Vander Aa <vanderaa@imec.be>
2017-09-21 15:10:05 +0200

    conda recipe seems to work on macOS

--- lib/smurff-cpp/conda-recipes/smurff-0.5.0/build.sh
+++ lib/smurff-cpp/conda-recipes/smurff-0.5.0/build.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 cd lib/macau-cpp/openmp
+
 export CPATH=$CPATH:$PREFIX/include/eigen3
+export CPATH=$CPATH:/usr/local/include
 export LIBRARY_PATH=$LIBRARY_PATH:$PREFIX/lib
+export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/lib
+
 sed -i -e 's/^LIBLOCS/#LIBLOCS/g' Makefile
-#sed -i -e 's/^CXX/#CXX/g' Makefile
+sed -i -e 's/^CXX/#CXX/g' Makefile
 #sed -i -e '/argp/d' Makefile
 
-make -j
-install cmd_nompi $PREFIX/bin
+if [ -n "$OSX_ARCH" ]
+then
+    make -j CXX=g++-6
+else 
+    make -j
+fi
+
+install cmd_nompi $PREFIX/bin/smurff
