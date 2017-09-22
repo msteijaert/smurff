0ac5c94021e1cccb92a9b1175df1cd88896c11a2
Tom Vander Aa <vanderaa@imec.be>
2017-09-22 11:23:11 +0200

    conda fixes

--- lib/smurff-cpp/conda-recipes/smurff-0.5.0/build.sh
+++ lib/smurff-cpp/conda-recipes/smurff-0.5.0/build.sh
@@ -5,7 +5,7 @@ cd lib/macau-cpp/openmp
 sed -i -e 's/^LIBLOCS/#LIBLOCS/g' Makefile
 sed -i -e 's/^CXX/#CXX/g' Makefile
 
-echo "CXXFLAGS+=-I/usr/loca/include" >> Makefile
+echo "CXXFLAGS+=-I/usr/local/include" >> Makefile
 echo "CXXFLAGS+=-I${PREFIX}/include/eigen3" >> Makefile
 echo "LDFLAGS+=-L${PREFIX}/lib -lopenblas" >> Makefile
 echo 'smurff: cmd_nompi.o $(LIBMACAU) cmd_session.o ' >> Makefile
@@ -14,7 +14,7 @@ echo '	$(CXX) $(CXXFLAGS) $(OUTPUT_OPTIONS) $? -o smurff  $(LDFLAGS)' >> Makefil
 
 if [ -n "$OSX_ARCH" ]
 then
-    make -j smurff CXX=g++-6 
+    make -j smurff
 else 
     sed -i -e '/argp/d' Makefile
     make -j smurff
