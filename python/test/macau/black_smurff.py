8c172531e6f03c96c9c8681034ef14ac13cf9f80
Tom Vander Aa <vanderaa@imec.be>
2017-09-28 16:23:24 +0200

    black smurff update

--- python/test/macau/black_smurff.py
+++ python/test/macau/black_smurff.py
@@ -1,77 +1,104 @@
 #!/usr/bin/env python3
 
 from macau import macau
+import matrix_io as mio
 import scipy.io
 import argparse
 
-parser = argparse.ArgumentParser(description='Process some integers.')
-# parser.add_argument('integers', metavar='N', type=int, nargs='+',
-#                    help='an integer for the accumulator')
-# parser.add_argument('--sum', dest='accumulate', action='store_const',
-#                     const=sum, default=max,
-#                     help='sum the integers (default: find the max)')
+parser = argparse.ArgumentParser(description='Black SMURFF - a smurff-compatible commandline for Macau')
 
-
-options = [
-        { "group": "Priors and side Info"},
-        { "name": "row-prior",	      "metavar": "PRIOR", "default": "normal", "help": "One of <normal|spikeandslab|macau|macauone>"},
-        { "name": "col-prior",	      "metavar": "PRIOR", "default": "normal", "help": "One of <normal|spikeandslab|macau|macauone>"},
-        { "name": "row-features",     "metavar": "FILE",  "help": "side info for rows"},
-        { "name": "col-features",     "metavar": "FILE",  "help": "side info for cols"},
-        { "name": "row-model",        "metavar": "FILE",  "help": "initialization matrix for row model"},
-        { "name": "col-model",        "metavar": "FILE",  "help": "initialization matrix for col model"},
-        { "name": "center",           "metavar": "MODE",  "default": "none", "help": "center <global|rows|cols|none>"},
-        { "group": "Test and train matrices"},
-        { "name": "test",	      "metavar": "FILE", "help":  "test data (for computing RMSE}"},
-        { "name": "train",	      "metavar": "FILE", "help":  "train data file", "required": True},
-        { "group": "General parameters"},
-        { "name": "burnin",	      "metavar": "NUM", "type": float,  "default": 200,  "help":  "200  number of samples to discard"},
-        { "name": "nsamples",	      "metavar": "NUM", "type": float,   "default": 800, "help":  "800  number of samples to collect"},
-        { "name": "num-latent",	      "metavar": "NUM", "type": float,   "default": 96, "help":  "96  number of latent dimensions"},
-        { "name": "restore-prefix",   "metavar": "PATH", "help":  "prefix for file to initialize stae"},
-        { "name": "restore-suffix",   "metavar": "EXT",  "help":  "suffix for initialization files (.csv or .ddm}"},
-        { "name": "init-model",       "metavar": "NAME", "help":  "One of <random|zero>"},
-        { "name": "save-prefix",      "metavar": "PATH", "help":  "prefix for result files"},
-        { "name": "save-suffix",      "metavar": "EXT",  "help":  "suffix for result files (.csv or .ddm}"},
-        { "name": "save-freq",        "metavar": "NUM", "type": float,  "help":  "save every n iterations (0 == never}"},
-        { "name": "threshold",        "metavar": "NUM", "type": float,  "help":  "threshold for binary classification"},
-        { "name": "verbose",          "metavar": "NUM", "nargs": "?", "type": float,  "help":  "verbose output (default = 1}"},
-        { "name": "quiet",                               "help":  "no output"},
-        { "name": "status",           "metavar": "FILE", "help":  "output progress to csv file"},
-        { "group": "Noise model"},
-        { "name": "precision",	      "metavar": "NUM", "type": float,  "default": 5.0,  "help":  "5.0  precision of observations"},
-        { "name": "adaptive",	      "metavar": "NUM,NUM",  "default": "1.0,10.0",  "help":  "1.0,10.0  adavtive precision of observations"},
-        { "group": "For the macau prior"},
-        { "name": "lambda-beta",      "metavar": "NUM", "type": float,  "default": 10.0, "help":  "10.0  initial value of lambda beta"},
-        { "name": "tol",              "metavar": "NUM", "type": float,   "default": 1e-6, "help":  "1e-6  tolerance for CG"},
-        { "name": "direct",           "default": False, "help":  "Use Cholesky decomposition i.o. CG Solver"},
-]
+options = {
+        "group1":         { "group": "Priors and side Info"},
+        "row-prior":  	  { "metavar": "PRIOR", "default": "normal", "help": "One of <normal|spikeandslab|macau|macauone>"},
+        "col-prior":  	  { "metavar": "PRIOR", "default": "normal", "help": "One of <normal|spikeandslab|macau|macauone>"},
+        "row-features":   { "metavar": "FILE",  "help": "side info for rows"},
+        "col-features":   { "metavar": "FILE",  "help": "side info for cols"},
+        #unsupported "row-model":      { "metavar": "FILE",  "help": "initialization matrix for row model"},
+        #unsupported "col-model":      { "metavar": "FILE",  "help": "initialization matrix for col model"},
+        #unsupported "center":         { "metavar": "MODE",  "default": "none", "help": "center <global|rows|cols|none>"},
+        "group2":         { "group": "Test and train matrices"},
+        "test":  	  { "metavar": "FILE", "help":  "test data (for computing RMSE}"},
+        "train":  	  { "metavar": "FILE", "help":  "train data file", "required": True},
+        "group3":         { "group": "General parameters"},
+        "burnin":  	  { "metavar": "NUM", "type": int,  "default": 200,  "help":  "200  number of samples to discard"},
+        "nsamples":  	  { "metavar": "NUM", "type": int,   "default": 800, "help":  "800  number of samples to collect"},
+        "num-latent":  	  { "metavar": "NUM", "type": int,   "default": 96, "help":  "96  number of latent dimensions"},
+        #unsupported "restore-prefix": { "metavar": "PATH", "help":  "prefix for file to initialize stae"},
+        #unsupported "restore-suffix": { "metavar": "EXT",  "help":  "suffix for initialization files (.csv or .ddm}"},
+        #unsupported "init-model":     { "metavar": "NAME", "help":  "One of <random|zero>"},
+        "save-prefix":    { "metavar": "PATH", "help":  "prefix for result files"},
+        #unsupported "save-suffix":    { "metavar": "EXT",  "help":  "suffix for result files (.csv or .ddm}"},
+        #unsupported "save-freq":      { "metavar": "NUM", "type": int,  "help":  "save every n iterations (0 == never}"},
+        #unsupported "threshold":      { "metavar": "NUM", "type": float,  "help":  "threshold for binary classification"},
+        "verbose":        { "metavar": "NUM", "nargs": "?", "type": int,  "help":  "verbose output (default = 1}"},
+        "quiet":          {                    "help":  "no output"},
+        #unsupported "status":         { "metavar": "FILE", "help":  "output progress to csv file"},
+        "group4":         { "group": "Noise model" },
+        "precision":  	  { "metavar": "NUM", "type": float, "help":  "precision of observations"},
+        "adaptive":  	  { "metavar": "NUM,NUM", "nargs": "?", "help":  "1.0,10.0  adaptive precision of observations"},
+        "group1":         { "group": "For the macau prior"},
+        "lambda-beta":    { "metavar": "NUM", "type": float,  "default": 10.0, "help":  "10.0  initial value of lambda beta"},
+        "tol":            { "metavar": "NUM", "type": float,   "default": 1e-6, "help":  "1e-6  tolerance for CG"},
+        #unsupported "direct":         { "default": False, "help":  "Use Cholesky decomposition i.o. CG Solver"},
+}
 
 group = parser
-for o in options:
-    if ("group" in o):
-        group = parser.add_argument_group(o["group"])
+for name, opt in options.items():
+    if ("group" in opt):
+        group = parser.add_argument_group(opt["group"])
     else:
-        name = '--' + o["name"]
-        del o["name"]
-        group.add_argument(name, **o)
+        group.add_argument("--" + name, **opt)
 
 args = parser.parse_args()
 
-train_matrix = scipy.io.mmread(args.train)
-test_matrix = None
-if (args.test):
-    test_matrix  = scipy.io.mmread(args.test)
-side_rows_matrix = None
-side_cols_matrix = None
+def read_matrix(fname):
+    if (not fname):
+        return None
+    if fname.endswith('.mtx') or fname.endswith('.mm'):
+        return scipy.io.mmread(fname)
+    elif fname.endswith('.sbm'):
+        return mio.read_sparse_binary_matrix(fname)
+    elif fname.endswith('.sdm'):
+        return mio.read_sparse_float64(fname)
+    elif fname.endswith('.ddm'):
+        return mio.read_dense_float64(fname)
+    else:
+        raise Exception("Unknown matrix type: " + fname)
 
+ 
 
-result = macau(Y          = train_matrix,
-               Ytest      = test_matrix,
-               num_latent = args.num_latent,
+train_matrix     = read_matrix(args.train)
+test_matrix      = read_matrix(args.test)
+side_rows_matrix = read_matrix(args.row_features)
+side_cols_matrix = read_matrix(args.col_features)
+
+# deduce noise model to use
+if ("adaptive" in vars(args)):
+    try:
+        args.sn_max = float(args.adaptive.split(",")[1])
+    except:
+        args.sn_max = 10.0
+    assert(not args.precision)
+    args.precision = "adaptive"
+else:
+    args.sn_max = 10.0
+    if (not args.precision):
+        args.precision = 5.0
+
+print(vars(args))
+
+result = macau(Y           = train_matrix,
+               Ytest       = test_matrix,
                side = [ side_rows_matrix, side_cols_matrix ],
-               verbose    = args.verbose,
-               precision  = args.precision,
-               burnin     = args.burnin,
-               nsamples   = args.nsamples)
+               lambda_beta = args.lambda_beta,
+               num_latent  = args.num_latent,
+               precision   = args.precision,
+               burnin      = args.burnin,
+               nsamples    = args.nsamples,
+               tol         = args.tol,
+               sn_max      = args.sn_max,
+               verbose     = args.verbose,
+               save_prefix = args.save_prefix)
 
+if (args.verbose):
+    print(result)
