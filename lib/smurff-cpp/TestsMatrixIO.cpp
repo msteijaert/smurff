67f3369bd5a31c0efb3a414c1c58ad6aee1e8a8c
Ilya Pasechnikov <ilya.pasechnikov@arcadia.spb.ru>
2017-09-29 15:02:30 +0300

    Add TestsMatrixIO.cpp file

---
+++ lib/smurff-cpp/TestsMatrixIO.cpp
@@ -0,0 +1,58 @@
+#include "catch.hpp"
+
+/*
+TEST_CASE("matrix_io/read_matrix | matrix_io/write_matrix")
+{
+   REQUIRE(false);
+}
+
+TEST_CASE("matrix_io/read_dense_float64_bin | matrix_io/write_dense_float64_bin")
+{
+   REQUIRE(false);
+}
+
+TEST_CASE("matrix_io/read_dense_float64_csv | matrix_io/write_dense_float64_csv")
+{
+   REQUIRE(false);
+}
+
+TEST_CASE("matrix_io/read_sparse_float64_bin | matrix_io/write_sparse_float64_bin")
+{
+   REQUIRE(false);
+}
+
+TEST_CASE("matrix_io/read_sparse_float64_mtx | matrix_io/write_sparse_float64_mtx")
+{
+   REQUIRE(false);
+}
+
+TEST_CASE("matrix_io/read_sparse_binary_bin | matrix_io/write_sparse_binary_bin")
+{
+   REQUIRE(false);
+}
+
+TEST_CASE("matrix_io/eigen::read_dense_float64_bin | matrix_io/eigen::write_dense_float64_bin")
+{
+   REQUIRE(false);
+}
+
+TEST_CASE("matrix_io/eigen::read_dense_float64_csv | matrix_io/eigen::write_dense_float64_csv")
+{
+   REQUIRE(false);
+}
+
+TEST_CASE("matrix_io/eigen::read_sparse_float64_bin | matrix_io/eigen::write_sparse_float64_bin")
+{
+   REQUIRE(false);
+}
+
+TEST_CASE("matrix_io/eigen::read_sparse_float64_mtx | matrix_io/eigen::write_sparse_float64_mtx")
+{
+   REQUIRE(false);
+}
+
+TEST_CASE("matrix_io/eigen::read_sparse_binary_bin | matrix_io/eigen::write_sparse_binary_bin")
+{
+   REQUIRE(false);
+}
+*/
