827d4efe905d60c69714fa814d663860f7503292
Tom Vander Aa <vanderaa@imec.be>
2017-10-02 12:47:17 +0200

    Fix conda recipes

--- conda-recipes/macau-0.5.0/build.sh
+++ conda-recipes/macau-0.5.0/build.sh
@@ -1,2 +1,4 @@
 sed -i -e 's/library_dirs = ldirs/library_dirs = []/g' setup.py
+sed -i -e 's#, "/usr/local/include", "/usr/local/opt/openblas/include"##g' setup.py
+sed -i -e "s#'-fopenmp'#'-fopenmp', '-DEIGEN_DONT_PARALLELIZE'#g" setup.py
 $PYTHON setup.py install     # Python command to install the script.
