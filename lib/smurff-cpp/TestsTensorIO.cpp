7b40b80d90dcc8a8300ecf181f4a778c86350833
Andrei Gedich <andrey.gedich@arcadia.spb.ru>
2017-10-05 17:19:51 +0300

    fix tensor io tests

--- lib/smurff-cpp/TestsTensorIO.cpp
+++ lib/smurff-cpp/TestsTensorIO.cpp
@@ -11,106 +11,85 @@ using namespace smurff;
 
 TEST_CASE("tensor_io/read_dense_float64_bin | tensor_io/write_dense_float64_bin")
 {
-   std::uint64_t matrixConfigNRow = 3;
-   std::uint64_t matrixConfigNCol = 3;
-   std::vector<double> matrixConfigValues = { 1, 4, 7, 2, 5, 8, 3, 6, 9 };
-   MatrixConfig matrixConfig( matrixConfigNRow
-                            , matrixConfigNCol
-                            , std::move(matrixConfigValues)
-                            , NoiseConfig()
-                            );
+   std::vector<std::uint64_t> tensorConfigDims = { 3, 4 };
+   std::vector<double> tensorConfigValues = { 1, 5, 9, 2, 6, 10, 3, 7, 11, 4, 8, 12 };
+   TensorConfig tensorConfig(std::move(tensorConfigDims), std::move(tensorConfigValues), NoiseConfig());
 
    std::stringstream matrixConfigStream;
-   tensor_io::write_dense_float64_bin(matrixConfigStream, matrixConfig);
+   tensor_io::write_dense_float64_bin(matrixConfigStream, tensorConfig);
 
    TensorConfig actualTensorConfig = tensor_io::read_dense_float64_bin(matrixConfigStream);
-   MatrixConfig actualMatrixConfig( matrixConfigNRow
-                                  , matrixConfigNCol
-                                  , actualTensorConfig.getColumns()
-                                  , actualTensorConfig.getValues()
-                                  , NoiseConfig()
-                                  );
-   Eigen::MatrixXd actualMatrix = sparse_to_eigen(actualMatrixConfig);
+   MatrixConfig actualMatrixConfig = tensor_to_matrix(actualTensorConfig);
+   
+   Eigen::MatrixXd actualMatrix0 = sparse_to_eigen(actualTensorConfig);
+   Eigen::MatrixXd actualMatrix1 = sparse_to_eigen(actualMatrixConfig);
 
-   Eigen::MatrixXd expectedMatrix(3, 3);
-   expectedMatrix << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+   Eigen::MatrixXd expectedMatrix(3, 4);
+   expectedMatrix << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12;
 
-   REQUIRE(actualMatrix.isApprox(expectedMatrix));
+   REQUIRE(actualMatrix0.isApprox(expectedMatrix));
+   REQUIRE(actualMatrix1.isApprox(expectedMatrix));
 }
 
 TEST_CASE("tensor_io/read_sparse_float64_bin | tensor_io/write_sparse_float64_bin")
 {
-   std::uint64_t matrixConfigNRow = 3;
-   std::uint64_t matrixConfigNCol = 3;
-   std::vector<std::uint32_t> matrixConfigRows = { 0, 0, 0, 2, 2, 2 };
-   std::vector<std::uint32_t> matrixConfigCols = { 0, 1, 2, 0, 1, 2 };
-   std::vector<double> matrixConfigValues      = { 1, 2, 3, 7, 8, 9 };
-   MatrixConfig matrixConfig( matrixConfigNRow
-                            , matrixConfigNCol
-                            , std::move(matrixConfigRows)
-                            , std::move(matrixConfigCols)
-                            , std::move(matrixConfigValues)
-                            , NoiseConfig()
-                            );
+   std::vector<std::uint64_t> tensorConfigDims = { 3, 4 };
+   std::vector<std::uint32_t> tensorConfigColumns = { 0, 0, 0, 0, 2, 2, 2, 2, 0, 1, 2, 3, 0, 1, 2, 3 };
+   std::vector<double> tensorConfigValues = { 1, 2, 3, 4, 9, 10, 11, 12 };
+   TensorConfig tensorConfig( std::move(tensorConfigDims), std::move(tensorConfigColumns), std::move(tensorConfigValues), NoiseConfig());
 
    std::stringstream tensorStream;
-   tensor_io::write_sparse_float64_bin(tensorStream, matrixConfig);
-   TensorConfig actualTensorConfig = tensor_io::read_sparse_float64_bin(tensorStream);
-   MatrixConfig actualMatrixConfig( matrixConfigNRow
-                                  , matrixConfigNCol
-                                  , actualTensorConfig.getColumns()
-                                  , actualTensorConfig.getValues()
-                                  , NoiseConfig()
-                                  );
-   Eigen::SparseMatrix<double> actualMatrix = sparse_to_eigen(actualMatrixConfig);
+   tensor_io::write_sparse_float64_bin(tensorStream, tensorConfig);
 
-   Eigen::SparseMatrix<double> expectedMatrix(3, 3);
+   TensorConfig actualTensorConfig = tensor_io::read_sparse_float64_bin(tensorStream);
+   MatrixConfig actualMatrixConfig = tensor_to_matrix(actualTensorConfig);
+   
+   Eigen::SparseMatrix<double> actualMatrix0 = sparse_to_eigen(actualTensorConfig);
+   Eigen::SparseMatrix<double> actualMatrix1 = sparse_to_eigen(actualMatrixConfig);
+
+   Eigen::SparseMatrix<double> expectedMatrix(3, 4);
    std::vector<Eigen::Triplet<double> > expectedMatrixTriplets;
    expectedMatrixTriplets.push_back(Eigen::Triplet<double>(0, 0, 1));
    expectedMatrixTriplets.push_back(Eigen::Triplet<double>(0, 1, 2));
    expectedMatrixTriplets.push_back(Eigen::Triplet<double>(0, 2, 3));
-   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 0, 7));
-   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 1, 8));
-   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 2, 9));
+   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(0, 3, 4));
+   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 0, 9));
+   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 1, 10));
+   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 2, 11));
+   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 3, 12));
    expectedMatrix.setFromTriplets(expectedMatrixTriplets.begin(), expectedMatrixTriplets.end());
 
-   REQUIRE(actualMatrix.isApprox(expectedMatrix));
+   REQUIRE(actualMatrix0.isApprox(expectedMatrix));
+   REQUIRE(actualMatrix1.isApprox(expectedMatrix));
 }
 
 TEST_CASE("tensor_io/read_sparse_binary_bin | tensor_io/write_sparse_binary_bin")
 {
-   std::uint64_t matrixConfigNRow = 3;
-   std::uint64_t matrixConfigNCol = 3;
-   std::vector<std::uint32_t> matrixConfigRows = { 0, 0, 0, 2, 2, 2 };
-   std::vector<std::uint32_t> matrixConfigCols = { 0, 1, 2, 0, 1, 2 };
-   MatrixConfig matrixConfig( matrixConfigNRow
-                            , matrixConfigNCol
-                            , std::move(matrixConfigRows)
-                            , std::move(matrixConfigCols)
-                            , NoiseConfig()
-                            );
+   std::vector<std::uint64_t> tensorConfigDims = { 3, 4 };
+   std::vector<std::uint32_t> tensorConfigColumns = { 0, 0, 0, 0, 2, 2, 2, 2, 0, 1, 2, 3, 0, 1, 2, 3 };
+   TensorConfig tensorConfig(std::move(tensorConfigDims), std::move(tensorConfigColumns), NoiseConfig());
 
-   std::stringstream tensorConfigStream;
-   tensor_io::write_sparse_binary_bin(tensorConfigStream, matrixConfig);
+   std::stringstream tensorStream;
+   tensor_io::write_sparse_binary_bin(tensorStream, tensorConfig);
 
-   TensorConfig actualTensorConfig = tensor_io::read_sparse_binary_bin(tensorConfigStream);
-   MatrixConfig actualMatrixConfig( matrixConfigNRow
-                                  , matrixConfigNCol
-                                  , actualTensorConfig.getColumns()
-                                  , actualTensorConfig.getValues()
-                                  , NoiseConfig()
-                                  );
-   Eigen::SparseMatrix<double> actualMatrix = sparse_to_eigen(actualMatrixConfig);
+   TensorConfig actualTensorConfig = tensor_io::read_sparse_binary_bin(tensorStream);
+   MatrixConfig actualMatrixConfig = tensor_to_matrix(actualTensorConfig);
+   
+   Eigen::SparseMatrix<double> actualMatrix0 = sparse_to_eigen(actualTensorConfig);
+   Eigen::SparseMatrix<double> actualMatrix1 = sparse_to_eigen(actualMatrixConfig);
 
-   Eigen::SparseMatrix<double> expectedMatrix(3, 3);
+   Eigen::SparseMatrix<double> expectedMatrix(3, 4);
    std::vector<Eigen::Triplet<double> > expectedMatrixTriplets;
    expectedMatrixTriplets.push_back(Eigen::Triplet<double>(0, 0, 1));
    expectedMatrixTriplets.push_back(Eigen::Triplet<double>(0, 1, 1));
    expectedMatrixTriplets.push_back(Eigen::Triplet<double>(0, 2, 1));
+   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(0, 3, 1));
    expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 0, 1));
    expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 1, 1));
    expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 2, 1));
+   expectedMatrixTriplets.push_back(Eigen::Triplet<double>(2, 3, 1));
    expectedMatrix.setFromTriplets(expectedMatrixTriplets.begin(), expectedMatrixTriplets.end());
 
-   REQUIRE(actualMatrix.isApprox(expectedMatrix));
+   REQUIRE(actualMatrix0.isApprox(expectedMatrix));
+   REQUIRE(actualMatrix1.isApprox(expectedMatrix));
 }
