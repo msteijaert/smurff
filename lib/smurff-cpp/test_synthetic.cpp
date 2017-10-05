6c9226073436ecd057d560e6ea94e5841fd718c9
Tom Vander Aa <vanderaa@imec.be>
2017-10-05 15:44:51 +0200

    ENH: macau -> smurff

--- lib/smurff-cpp/test_synthetic.cpp
+++ lib/smurff-cpp/test_synthetic.cpp
@@ -28,60 +28,60 @@ const int num_latent = 5;
 void test_sparse(int N, int D, int iter_max) 
 {
     assert(D>0 && N>0 && iter_max > 0 && "Usage GFA N D iter_max");
-    Session macau;
-    auto &master_model = macau.sparseModel(num_latent);
-    macau.config.burnin = 10;
-    macau.config.nsamples = iter_max;
-    macau.config.verbose = true;
+    Session session;
+    auto &master_model = session.sparseModel(num_latent);
+    session.config.burnin = 10;
+    session.config.nsamples = iter_max;
+    session.config.verbose = true;
 
     // fixed gaussian noise
-    macau.setPrecision(1.0);
+    session.setPrecision(1.0);
 
     // = random_Ydense(N,D,3);
     auto Ytrain = ones_Ysparse(N,D,2,.8);
     auto predictions = extract(Ytrain,.2);
     master_model.set(Ytrain);
-    macau.pred.set(predictions);
+    session.pred.set(predictions);
 
     //-- Normal priors
-    macau.addPrior<SpikeAndSlabPrior>();
-    //macau.addPrior<NormalPrior>();
-    auto &master_prior = macau.addPrior<MasterPrior<NormalPrior>>();
+    session.addPrior<SpikeAndSlabPrior>();
+    //session.addPrior<NormalPrior>();
+    auto &master_prior = session.addPrior<MasterPrior<NormalPrior>>();
     auto &slave_model = master_prior.addSlave<ScarceMatrixData>();
     auto Y1 = ones_Ysparse(N,2*D,2,.3);
     slave_model.set(Y1);
 
-    macau.run();
+    session.run();
 }
 
 void test_dense_dense(int N, int D, int iter_max) 
 {
     assert(D>0 && N>0 && iter_max > 0 && "Usage GFA N D iter_max");
-    Session macau;
-    auto &master_model = macau.denseDenseModel(num_latent);
-    macau.config.burnin = 10;
-    macau.config.nsamples = iter_max;
-    macau.config.verbose = true;
+    Session session;
+    auto &master_model = session.denseDenseModel(num_latent);
+    session.config.burnin = 10;
+    session.config.nsamples = iter_max;
+    session.config.verbose = true;
 
     // fixed gaussian noise
-    macau.setPrecision(1.0);
+    session.setPrecision(1.0);
 
     // = random_Ydense(N,D,3);
     auto Ytrain2 = ones_Ydense(N,D,2);
     auto predictions2  = extract(Ytrain2, .2);
     master_model.set(Ytrain2);
-    macau.pred.set(predictions2);
+    session.pred.set(predictions2);
 
     //-- Normal priors
-    macau.addPrior<SpikeAndSlabPrior>();
-    //macau.addPrior<NormalPrior>();
-    //macau.addPrior<NormalPrior>();
-    auto &master_prior = macau.addPrior<MasterPrior<NormalPrior>>();
+    session.addPrior<SpikeAndSlabPrior>();
+    //session.addPrior<NormalPrior>();
+    //session.addPrior<NormalPrior>();
+    auto &master_prior = session.addPrior<MasterPrior<NormalPrior>>();
     auto &slave_model = master_prior.addSlave<DenseMatrixData>();
     auto Ytrain1 = ones_Ydense(N,2*D,2);
     slave_model.set(Ytrain1);
 
-    macau.run();
+    session.run();
 }
 
 int main(int argc, char** argv) {
