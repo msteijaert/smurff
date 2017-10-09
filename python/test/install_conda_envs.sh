48168eeac49e151cc7e60667d1309a30e5a64842
Tom Vander Aa <vanderaa@imec.be>
2017-10-09 15:13:39 +0200

    Add smurff 0.8.1

--- python/test/install_conda_envs.sh
+++ python/test/install_conda_envs.sh
@@ -8,7 +8,7 @@ cd conda_envs
 conda create -p smurff-0.5.0 -c vanderaa --yes smurff=0.5.0 scipy pandas scikit-learn
 conda create -p smurff-0.6.1 -c vanderaa --yes smurff=0.6.1 scipy pandas scikit-learn
 conda create -p smurff-0.7.0 -c vanderaa --yes smurff=0.7.0 scipy pandas scikit-learn
-conda create -p smurff-0.8.0 -c vanderaa --yes smurff=0.8.0 scipy pandas scikit-learn
+conda create -p smurff-0.8.1 -c vanderaa --yes smurff=0.8.1 scipy pandas scikit-learn
 conda create -p macau-0.5.0  -c vanderaa --yes macau=0.5.0 black_smurff scipy pandas scikit-learn
 
 cd ..
