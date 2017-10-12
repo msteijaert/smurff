5aafe5d3cca158434f2eea6b2ae6a57c1683c8a7
Tom Vander Aa <vanderaa@imec.be>
2017-10-12 10:15:15 +0200

    matrix_io package

---
+++ python/io/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='matrix_io',
+        version='0.1',
+        description='Reading and writing sparse and dense matrices in binary format',
+        url='http://github.com/ExaScience/smurff',
+        author='Tom Vander Aa',
+        author_email='tom.vanderaa@imec.be',
+        license='MIT',
+        packages=['matrix_io'],
+        install_requires=[ 'scipy' ],
+        scripts=['bin/csv2ddm', 'bin/mtx2ddm', 'bin/mtx2sbm', 'bin/mtx2sdm' ]
+        zip_safe=False)
