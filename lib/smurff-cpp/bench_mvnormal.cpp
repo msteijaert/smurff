019b98fe76a9fc4bae1d457e39248b7da267e51a
Andrei Gedich <andrey.gedich@arcadia.spb.ru>
2017-10-13 18:52:01 +0300

    move mv benchmarking code to separate files.

---
+++ lib/smurff-cpp/bench_mvnormal.cpp
@@ -0,0 +1,32 @@
+#include <Utils/Distribution.h>
+#include <Utils/utils.h>
+
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+   smurff::init_bmrng(1234);
+   {
+      MatrixXd U(32,32 * 1024);
+      U.setOnes();
+
+      VectorXd mu(32);
+      mu.setZero();
+
+      MatrixXd T(32,32);
+      T.setIdentity(32,32);
+      T.array() /= 4;
+
+      VectorXd mu_out;
+      MatrixXd T_out;
+
+      cout << "MVNORMAL\n" << endl;
+      MatrixXd out = smurff::MvNormal(T, mu, 10);
+      cout << "mu:\n" << mu << endl;
+      cout << "T:\n" << T << endl;
+      cout << "out:\n" << out << endl;
+   }
+
+   return 0;
+}
---
+++ lib/smurff-cpp/bench_wishart.cpp
@@ -0,0 +1,45 @@
+#include <Utils/Distribution.h>
+#include <Utils/utils.h>
+
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+   smurff::init_bmrng(1234);
+   {
+      MatrixXd U(32,32 * 1024);
+      U.setOnes();
+
+      VectorXd mu(32);
+      mu.setZero();
+
+      double kappa = 2;
+
+      MatrixXd T(32,32);
+      T.setIdentity(32,32);
+      T.array() /= 4;
+
+      int nu = 3;
+
+      VectorXd mu_out;
+      MatrixXd T_out;
+
+      cout << "COND NORMAL WISHART\n" << endl;
+
+      tie(mu_out, T_out) = smurff::CondNormalWishart(U, mu, kappa, T, nu);
+
+      cout << "mu_out:\n" << mu_out << endl;
+      cout << "T_out:\n" << T_out << endl;
+
+      cout << "\n-----\n\n";
+
+      cout << "NORMAL WISHART\n" << endl;
+
+      tie(mu_out, T_out) = smurff::NormalWishart(mu, kappa, T, nu);
+      cout << "mu_out:\n" << mu_out << endl;
+      cout << "T_out:\n" << T_out << endl;
+   }
+
+   return 0;
+}
