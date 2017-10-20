4c14bba976ad96aad569a6e0ac2100e29a6348d3
Ilya Pasechnikov <ilya.pasechnikov@arcadia.spb.ru>
2017-10-20 12:41:36 +0300

    Update matrix_utils::slice tests for 3D tensor

--- lib/smurff-cpp/Tests/TestsMatrixUtils.cpp
+++ lib/smurff-cpp/Tests/TestsMatrixUtils.cpp
@@ -7,7 +7,7 @@
 
 using namespace smurff;
 
-TEST_CASE("matrix_utils::slice. 3D tensor")
+TEST_CASE("matrix_utils::slice : 3D tensor")
 {
    std::vector<std::uint64_t> tensorConfigDims = { 2, 3, 4 };
    std::vector<std::uint32_t> tensorConfigColumns =
@@ -44,6 +44,7 @@ TEST_CASE("matrix_utils::slice. 3D tensor")
                             , { 1, 0 }
                             , {{ 2, 3 }}
                             );
+
       Eigen::MatrixXd expectedTensorSlice(3, 2);
       expectedTensorSlice << 18, 21,
                              19, 22,
@@ -59,6 +60,7 @@ TEST_CASE("matrix_utils::slice. 3D tensor")
                             , { 1, 2 }
                             , {{ 0, 1 }}
                             );
+
       Eigen::MatrixXd expectedTensorSlice(3, 4);
       expectedTensorSlice << 3,  9, 15, 21,
                              4, 10, 16, 22,
@@ -70,10 +72,11 @@ TEST_CASE("matrix_utils::slice. 3D tensor")
    // 3rd and 2nd dimension slice. Matrix with 4 rows and 3 cols
    {
       Eigen::MatrixXd actualTensorSlice =
-      matrix_utils::slice( tensorConfig
-                         , { 2, 1 }
-                         , {{ 0, 1 }}
-                         );
+         matrix_utils::slice( tensorConfig
+                            , { 2, 1 }
+                            , {{ 0, 1 }}
+                            );
+
       Eigen::MatrixXd expectedTensorSlice(4, 3);
       expectedTensorSlice <<  3,  4,  5,
                               9, 10, 11,
@@ -85,11 +88,38 @@ TEST_CASE("matrix_utils::slice. 3D tensor")
 
    // 1st and 3rd dimension slice. Matrix with 2 rows and 4 cols
    {
-      // TODO
+      Eigen::MatrixXd actualTensorSlice =
+         matrix_utils::slice( tensorConfig
+                            , { 0, 2 }
+                            , {{ 1, 1 }}
+                            );
+
+      Eigen::MatrixXd expectedTensorSlice(2, 4);
+      expectedTensorSlice << 1,  7, 13, 19,
+                             4, 10, 16, 22;
+
+      REQUIRE(matrix_utils::equals(actualTensorSlice, expectedTensorSlice));
    }
 
    // 3rd and 1st dimension slice. Matrix with 4 rows and 2 cols
    {
-      // TODO
+      Eigen::MatrixXd actualTensorSlice =
+         matrix_utils::slice( tensorConfig
+                            , { 2, 0 }
+                            , {{ 1, 1 }}
+                            );
+
+      Eigen::MatrixXd expectedTensorSlice(4, 2);
+      expectedTensorSlice <<  1,  4,
+                              7, 10,
+                             13, 16,
+                             19, 22;
+
+      REQUIRE(matrix_utils::equals(actualTensorSlice, expectedTensorSlice));
    }
+}
+
+TEST_CASE("matrix_utils::slice : 6D tensor")
+{
+   // TODO
 }
