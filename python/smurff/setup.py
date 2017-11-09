888051f40fe1e50e4c7c936ea6946884add59c77
Ilya Pasechnikov <ilya.pasechnikov@arcadia.spb.ru>
2017-11-09 11:35:57 +0300

    Use relative paths in setup.py

--- python/smurff/setup.py
+++ python/smurff/setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
-SOURCES = ["/home/ipasechnikov/smurff-python/smurff.pyx"]
-INCLUDE_DIRS = ["/home/ipasechnikov/smurff/lib/smurff-cpp", "/home/ipasechnikov/smurff/lib/eigen3"]
+SOURCES = ["smurff.pyx"]
+INCLUDE_DIRS = ["../../lib/smurff-cpp", "../../lib/eigen3"]
 LIBRARIES = ["blas"]
 
 setup(
@@ -12,7 +12,7 @@ setup(
 				    sources=SOURCES,
                 include_dirs = INCLUDE_DIRS,
                 libraries=LIBRARIES,
-                extra_objects=["/home/ipasechnikov/smurff/lib/smurff-cpp/cmake/build/libraries/SmurffCpp/libsmurff-cpp.a"],
+                extra_objects=["../../lib/smurff-cpp/cmake/build/libraries/SmurffCpp/libsmurff-cpp.a"],
                 extra_compile_args=['-std=c++11'],
                 language="c++")
       ])
