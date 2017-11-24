1eb904db6fd921f1e6f6fe974708023b6b87d363
Tom Vander Aa <vanderaa@imec.be>
2017-11-24 14:54:52 +0100

    centering python script fixes

--- python/test/center.py
+++ python/test/center.py
@@ -20,17 +20,19 @@ print(args)
 train = mio.read_matrix(args.train)
 test = mio.read_matrix(args.test)
 
+assert train.shape == test.shape
+
 def avg_sparse_cols(m):
     m = m.tocsc(copy=False) 
     sums = m.sum(axis=0).A1
     counts = np.diff(m.indptr)
-    avg = sums / (counts + 0.000001)
-    avg = np.expand_dims(avg, 0)
-    sio.mmwrite("col_avg", avg)
-    return np.broadcast_to(avg, m.shape)
+    counts[counts == 0] = 1
+    avg = sums / counts
+    sio.mmwrite("col_avg", np.expand_dims(avg, 0))
+    return avg
  
 def center_sparse_cols(m, avg):
-    m = m.tocsc(copy=False) 
+    m = m.tocsr(copy=False) 
     m.data -= np.take(avg, m.indices)
     return m
 
@@ -38,13 +40,13 @@ def avg_sparse_rows(m):
     m = m.tocsr(copy=False) 
     sums = m.sum(axis=1).A1
     counts = np.diff(m.indptr)
-    avg = sums / (counts + 0.000001)
-    avg = np.expand_dims(avg, 1)
-    sio.mmwrite("row_avg", avg)
-    return np.broadcast_to(avg, m.shape)
+    counts[counts == 0] = 1
+    avg = sums / counts
+    sio.mmwrite("row_avg", np.expand_dims(avg, 1))
+    return avg
 
 def center_sparse_rows(m,avg):
-    m = m.tocsr(copy=False) 
+    m = m.tocsc(copy=False) 
     m.data -= np.take(avg, m.indices)
     return m
 
@@ -64,17 +66,15 @@ if (sparse.issparse(train)):
     elif (args.mode != "none"):
         raise ValueError("Unknown centering mode: %s" % ( args.mode ) )
 else:
-    if (args.mode == "rows"): 
+    if (args.mode == "cols"): 
         avg = np.mean(train, 0)
-        avg = np.expand_dims(avg, 1)
-        sio.mmwrite("row_avg", avg)
-        train = train - np_broadcast_to(avg, train.shape)
+        sio.mmwrite("col_avg", np.expand_dims(avg, 0))
+        train = train - np.broadcast_to(np.expand_dims(avg, 0), train.shape)
         test = center_sparse_rows(test,avg)
-    elif (args.mode == "cols"):
+    elif (args.mode == "rows"):
         avg = np.mean(train, 1)
-        avg = np.expand_dims(avg, 1)
-        sio.mmwrite("col_avg", avg)
-        train = train - np_broadcast_to(avg, train.shape)
+        sio.mmwrite("row_avg", np.expand_dims(avg, 1))
+        train = train - np.broadcast_to(np.expand_dims(avg, 1), train.shape)
         test = center_sparse_cols(test,avg)
     elif (args.mode == "global"): 
         avg = np.mean(train)
@@ -83,6 +83,8 @@ else:
     elif (args.mode != "none"):
         raise ValueError("Unknown centering mode: %s" % ( args.mode ) )
 
-sio.mmwrite("centered_train", train)
-sio.mmwrite("centered_test", test)
+base, ext =  os.path.splitext(args.train)
+mio.write_matrix("centered_train" + ext, train)
+base, ext =  os.path.splitext(args.test)
+mio.write_matrix("centered_test" + ext, test)
 
