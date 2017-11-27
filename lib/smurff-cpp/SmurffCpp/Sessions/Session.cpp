f913814143323c4220b7b7ed863557b0b6187f83
Tom Vander Aa <vanderaa@imec.be>
2017-11-27 13:50:50 +0100

    correct naming for csv fields

--- lib/smurff-cpp/SmurffCpp/Sessions/Session.cpp
+++ lib/smurff-cpp/SmurffCpp/Sessions/Session.cpp
@@ -250,15 +250,15 @@ void Session::printStatus(double elapsedi)
 
    if (config.csv_status.size())
    {
-      double colmean_rmse = m_pred->rmse_using_modemean(data(), 0);
-      double globalmean_rmse = m_pred->rmse_using_modemean(data(), 1);
+      double rowmean_rmse = m_pred->rmse_using_modemean(data(), 0);
+      double colmean_rmse = m_pred->rmse_using_modemean(data(), 1);
       double train_rmse = data()->train_rmse(m_model);
 
       auto f = fopen(config.csv_status.c_str(), "a");
 
       fprintf(f, "%s;%d;%d;%.4f;%.4f;%.4f;%.4f;%.4f;%.4f;:%.4f;%1.2e;%1.2e;%0.1f\n",
             phase.c_str(), i, from,
-            globalmean_rmse, colmean_rmse,
+            rowmean_rmse, colmean_rmse,
             m_pred->rmse_avg, m_pred->rmse_1sample, train_rmse, m_pred->auc_1sample, m_pred->auc_avg, snorm0, snorm1, elapsedi);
 
       fclose(f);
