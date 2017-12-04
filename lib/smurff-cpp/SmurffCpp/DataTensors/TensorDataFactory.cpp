ea517baf6f7016664e3eec6b1ffea298241a1c70
Ilya Pasechnikov <ilya.pasechnikov@arcadia.spb.ru>
2017-12-04 15:31:14 +0300

    Fix NoiseModel issue in TensorDataFactory

--- lib/smurff-cpp/SmurffCpp/DataTensors/TensorDataFactory.cpp
+++ lib/smurff-cpp/SmurffCpp/DataTensors/TensorDataFactory.cpp
@@ -1,15 +1,21 @@
 #include "TensorDataFactory.h"
 
 #include <SmurffCpp/DataTensors/TensorData.h>
+#include <SmurffCpp/Noises/NoiseFactory.h>
 
 using namespace smurff;
 
 std::shared_ptr<Data> TensorDataFactory::create_tensor_data(std::shared_ptr<const TensorConfig> config,
-                                                            const std::vector<std::shared_ptr<MatrixConfig> >& row_features, 
+                                                            const std::vector<std::shared_ptr<MatrixConfig> >& row_features,
                                                             const std::vector<std::shared_ptr<MatrixConfig> >& col_features)
 {
    if (row_features.empty() && col_features.empty())
-      return std::make_shared<TensorData>(*config);
+   {
+      std::shared_ptr<TensorData> tensorData = std::make_shared<TensorData>(*config);
+      std::shared_ptr<INoiseModel> noise = NoiseFactory::create_noise_model(config->getNoiseConfig());
+      tensorData->setNoiseModel(noise);
+      return tensorData;
+   }
 
    throw std::runtime_error("Tensor config does not support feature matrices");
 }
