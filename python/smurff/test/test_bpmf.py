06210e71dbe20e548137f959daf4eb7ee25b6e7f
Ilya Pasechnikov <ilya.pasechnikov@arcadia.spb.ru>
2017-12-18 16:59:18 +0300

    Add missing test to test_bpmf.py

--- python/smurff/test/test_bpmf.py
+++ python/smurff/test/test_bpmf.py
@@ -6,6 +6,10 @@ import numpy as np
 class TestBPMF(unittest.TestCase):
     def test_bpmf(self):
         X = scipy.sparse.rand(15, 10, 0.2)
+        smurff.smurff(X, num_latent = 10, burnin=10, nsamples=15)
+
+    def test_bpmf2(self):
+        X = scipy.sparse.rand(15, 10, 0.2)
         Xt = scipy.sparse.rand(15, 10, 0.1)
         smurff.smurff(X, Xt, num_latent = 10, burnin=10, nsamples=15)
 
