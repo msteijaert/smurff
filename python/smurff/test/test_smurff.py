ecff2bd8f7eab0ebb41f24d67e9b0d2d555b09a0
Ilya Pasechnikov <ilya.pasechnikov@arcadia.spb.ru>
2017-12-20 16:38:17 +0300

    Enable 'test_bpmf_tensor' test in test_smurff.py

--- python/smurff/test/test_smurff.py
+++ python/smurff/test/test_smurff.py
@@ -105,23 +105,22 @@ class TestSmurff(unittest.TestCase):
 
         self.assertTrue(np.allclose(A1, A2))
 
-    # def test_bpmf_tensor(self):
-    #     np.random.seed(1234)
-    #     Y = pd.DataFrame({
-    #         "A": np.random.randint(0, 5, 7),
-    #         "B": np.random.randint(0, 4, 7),
-    #         "C": np.random.randint(0, 3, 7),
-    #         "value": np.random.randn(7)
-    #     })
-    #     Ytest = pd.DataFrame({
-    #         "A": np.random.randint(0, 5, 5),
-    #         "B": np.random.randint(0, 4, 5),
-    #         "C": np.random.randint(0, 3, 5),
-    #         "value": np.random.randn(5)
-    #     })
-    #     results = macau.bpmf(Y, Ytest = Ytest, num_latent = 4,
-    #                          verbose = False, burnin = 50, nsamples = 50,
-    #                          univariate = False)
+    def test_bpmf_tensor(self):
+        np.random.seed(1234)
+        Y = pd.DataFrame({
+            "A": np.random.randint(0, 5, 7),
+            "B": np.random.randint(0, 4, 7),
+            "C": np.random.randint(0, 3, 7),
+            "value": np.random.randn(7)
+        })
+        Ytest = pd.DataFrame({
+            "A": np.random.randint(0, 5, 5),
+            "B": np.random.randint(0, 4, 5),
+            "C": np.random.randint(0, 3, 5),
+            "value": np.random.randn(5)
+        })
+
+        results = smurff.smurff(Y, Ytest = Ytest, num_latent = 4, verbose = False, burnin = 50, nsamples = 50)
 
     # def test_bpmf_tensor2(self):
     #     A = np.random.randn(15, 2)
