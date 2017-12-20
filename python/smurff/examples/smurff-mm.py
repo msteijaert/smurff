811b3a213020170210cfa3f9cfe7bfc87c17856d
Ilya Pasechnikov <ilya.pasechnikov@arcadia.spb.ru>
2017-12-20 18:34:49 +0300

    Update smurff-mm.py example

--- python/smurff/examples/smurff-mm.py
+++ python/smurff/examples/smurff-mm.py
@@ -6,5 +6,5 @@ test_matrix_path = "chembl-IC50-test.mm"
 
 train = scipy.io.mmread(train_matrix_path)
 test = scipy.io.mmread(test_matrix_path)
-result_items = smurff(train, test, num_latent = 1, burnin=1, nsamples=5)
-print(result_items[0])
+result = smurff(train, test, num_latent = 1, burnin=1, nsamples=5)
+print(result.predictions[0])
