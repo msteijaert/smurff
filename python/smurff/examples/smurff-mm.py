2331362854eb9962488414859e5be83efd81a977
Ilya Pasechnikov <ilya.pasechnikov@arcadia.spb.ru>
2017-12-21 19:04:23 +0300

    Update smurff-mm.py example to use new side info argument

--- python/smurff/examples/smurff-mm.py
+++ python/smurff/examples/smurff-mm.py
@@ -6,5 +6,5 @@ test_matrix_path = "chembl-IC50-test.mm"
 
 train = scipy.io.mmread(train_matrix_path)
 test = scipy.io.mmread(test_matrix_path)
-result = smurff(train, test, num_latent = 1, burnin=1, nsamples=5)
+result = smurff(train, test, side=[('normal', []), ('normal', [])], num_latent = 1, burnin=1, nsamples=5)
 print(result.predictions[0])
