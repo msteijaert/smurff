4eda53448364229eb8511102361654c838d0e335
Tom Vander Aa <vanderaa@imec.be>
2018-01-12 09:15:01 +0100

    ENH: alpha protected

--- lib/smurff-cpp/SmurffCpp/Noises/GaussianNoise.h
+++ lib/smurff-cpp/SmurffCpp/Noises/GaussianNoise.h
@@ -10,9 +10,10 @@ namespace smurff {
    // Gaussian noise is fixed for the whole run
    class GaussianNoise : public INoiseModel
    {
-   public:
+   protected:
       double alpha = NAN;
 
+   public:
       void getMuLambda(const SubModel& model, int mode, int d, Eigen::VectorXd& rr, Eigen::MatrixXd& MM) override;
    };
 
