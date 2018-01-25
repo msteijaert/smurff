381bdac6bd4dd8421a1072ea65afef9825d49ca5
Tom Vander Aa <vanderaa@imec.be>
2018-01-25 15:51:53 +0000

    Update smurff-latest conda recipe

--- conda-recipes/smurff-latest/build.sh
+++ conda-recipes/smurff-latest/build.sh
@@ -5,6 +5,9 @@ cd lib/smurff-cpp/cmake
 rm -rf build 
 mkdir build
 cd build
-cmake ../ -DENABLE_OPENBLAS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX}  -DCMAKE_OSX_DEPLOYMENT_TARGET="" 
-make 
+cmake ../ -DENABLE_OPENBLAS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
+    -DENABLE_BOOST_RANDOM=ON \
+    -DBoost_USE_STATIC_LIBS:BOOL=OFF \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET="" 
+make -j${CPU_COUNT} 
 make install
