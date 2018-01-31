aaf9e15ef1a52a8e882ebedac113f0c7c1be604f
Tom Vander Aa <vanderaa@imec.be>
2018-01-31 08:57:14 +0100

    Simplified conda-recipe

--- conda-recipes/smurff-0.9.2/build.sh
+++ conda-recipes/smurff-0.9.2/build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-cd lib
-
-wget http://bitbucket.org/eigen/eigen/get/3.3.4.tar.bz2
-tar xjf 3.3.4.tar.bz2
-rm -rf eigen3
-mv eigen-eigen-5a0156e40feb eigen3
-
-cd smurff-cpp/cmake
-rm -rf build 
-mkdir build
-cd build
-cmake ../ -DENABLE_OPENBLAS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX}  -DCMAKE_OSX_DEPLOYMENT_TARGET="" 
-make 
+cd lib/smurff-cpp/cmake
+rm -rf build && mkdir build && cd build
+cmake ../ -DENABLE_OPENBLAS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PREFIX}" 
+make -j${CPU_COUNT}
 make install
