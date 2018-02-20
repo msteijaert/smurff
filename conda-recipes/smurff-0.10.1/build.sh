f7853a2611009ce677c981103afef8949a2bd1fa
Tom Vander Aa <vanderaa@imec.be>
2018-02-20 15:48:58 +0100

    Unified cmd-line and python package

--- conda-recipes/smurff-0.10.1/build.sh
+++ conda-recipes/smurff-0.10.1/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-cd lib/smurff-cpp/cmake
+pushd lib/smurff-cpp/cmake
 rm -rf build 
 mkdir build
 cd build
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX}  -DCMAKE_OSX_DEPLOYMENT_TARGET="" \
-     -DBOOST_ROOT=${CONDA_PREFIX} -DMPI_C_FOUND=NO
-
+cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX}  -DCMAKE_OSX_DEPLOYMENT_TARGET=""
 make 
 make install
+popd
+
+pushd python/smurff
+$PYTHON setup.py install --with-openmp --single-version-externally-managed --record=record.txt
+popd
