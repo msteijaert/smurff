d3a742dfd6a56ee869d40be3eeb5d204c9350e8e
Tom Vander Aa <vanderaa@imec.be>
2018-02-23 18:20:56 +0100

    conda fixes

--- conda-recipes/smurff-0.10.1/build.sh
+++ conda-recipes/smurff-0.10.1/build.sh
@@ -4,11 +4,11 @@ pushd lib/smurff-cpp/cmake
 rm -rf build 
 mkdir build
 cd build
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX}  -DCMAKE_OSX_DEPLOYMENT_TARGET=""
-make 
+cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DLAPACK_LIBRARIES=${CONDA_PREFIX}/lib/libmkl_rt${SHLIB_EXT}
+make -j$CPU_COUNT
 make install
 popd
 
 pushd python/smurff
-python setup.py install --with-openmp --single-version-externally-managed --record=record.txt
+$PYTHON setup.py install --with-openmp --single-version-externally-managed --record=record.txt
 popd
