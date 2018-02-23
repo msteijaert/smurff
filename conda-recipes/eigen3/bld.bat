db9f68131ee3f0a3df2f273d3e1d0fa654048cdd
Tom Vander Aa <vanderaa@imec.be>
2018-02-23 18:27:45 +0100

    eigen3 conda recipe for windows

---
+++ conda-recipes/eigen3/bld.bat
@@ -0,0 +1,13 @@
+@echo off
+
+mkdir build
+cd build
+
+set CMAKE_CONFIG="Release"
+
+cmake .. -LAH -G"%CMAKE_GENERATOR%"                       ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
+
+cmake --build . --config %CMAKE_CONFIG% --target ALL_BUILD
+cmake --build . --config %CMAKE_CONFIG% --target INSTALL
+if errorlevel 1 exit 1
