32308e60208b10555e265d5efb5ee370f5318d74
Tom Vander Aa <vanderaa@imec.be>
2018-03-02 10:33:37 +0100

    update conda recipe

---
+++ conda-recipes/smurff-0.10.2/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+test $(uname) == "Darwin" && CMAKE_OPENMP_FLAGS="-DOpenMP_CXX_FLAGS=-fopenmp=libiomp5 -DOpenMP_C_FLAGS=-fopenmp=libiomp5"
+ 
+pushd lib/smurff-cpp/cmake
+rm -rf build 
+mkdir build
+cd build
+cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DLAPACK_LIBRARIES=${PREFIX}/lib/libmkl_rt${SHLIB_EXT} ${CMAKE_OPENMP_FLAGS}
+make -j$CPU_COUNT
+make install
+popd
+
+pushd python/smurff
+$PYTHON setup.py install --with-openmp --single-version-externally-managed --record=record.txt
+popd
