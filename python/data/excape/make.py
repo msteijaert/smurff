9000e4a30976e1032ab2c37ba41e02f45700889d
Tom Vander Aa <vanderaa@imec.be>
2018-03-02 17:21:33 +0100

    1st ExCAPE test

--- python/data/excape/make.py
+++ python/data/excape/make.py
@@ -31,6 +31,12 @@ def save_response_content(response, destination):
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
 
+def sha256_from(filename):
+    with open(filename, "rb") as f:
+        sha = sha256(f.read()).hexdigest()
+
+    return sha
+
 def download():
     urls = [
             ( 
@@ -62,13 +68,15 @@ def download():
  
     for id, expected_sha, output in urls:
         if os.path.isfile(output):
-            actual_sha = sha256(open(output, "rb").read()).hexdigest()
+            actual_sha = sha256_from(output)
             if (expected_sha == actual_sha):
                 print("already have %s" % output)
                 continue
 
         print("download %s" % output)
         download_file_from_google_drive(id, output)
+        actual_sha = sha256_from(output)
+        assert (expected_sha == actual_sha)
 
 if __name__ == "__main__":
     download()
