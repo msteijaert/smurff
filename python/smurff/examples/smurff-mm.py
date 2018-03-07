ab62d2da96dbdb1aa0974fb5aa8e7f4ffc1faba2
Tom Vander Aa <vanderaa@imec.be>
2018-03-07 17:18:39 +0100

    fix example to work with smurff v0.10 (issue #101)

--- python/smurff/examples/smurff-mm.py
+++ python/smurff/examples/smurff-mm.py
@@ -6,5 +6,16 @@ test_matrix_path = "chembl-IC50-test.mm"
 
 train = scipy.io.mmread(train_matrix_path)
 test = scipy.io.mmread(test_matrix_path)
-result = smurff(train, test, side=[('normal', []), ('normal', [])], num_latent = 1, burnin=1, nsamples=5)
+
+result = smurff(
+        train,
+        Ytest = test,
+        priors = ["normal", "normal"],
+        side_info = [ None, None ],
+        aux_data =  [ [], [] ],
+        num_latent = 1,
+        burnin = 1,
+        nsamples = 5,
+        precision = 5.)
+
 print(result.predictions[0])
