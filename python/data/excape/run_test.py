16a8730fd550c59e643475ed4ee07fb4367ca82c
Tom Vander Aa <vanderaa@imec.be>
2018-03-09 13:54:24 +0100

    Clean run_test, all cmd-line versions

--- python/data/excape/run_test.py
+++ python/data/excape/run_test.py
@@ -11,8 +11,50 @@ from subprocess import call
 from time import time
 
 
+def smurff_py(args):
+    allowed_args = [ "Ytest", "num_latent", "burnin", "nsamples", "precision", "verbose", "aux_data", "side_info", "priors"]
+    filtered_args = { k: args[k] for k in allowed_args}
+    return smurff.smurff(args["train"], **filtered_args)
+
+def smurff_cmd(args):
+    args["side_info_string"] = " ".join(args["side_info_files"])
+    args["aux_data_string"] = " ".join(args["aux_data_files"])
+    args["prior_string"] = " ".join(args["priors"])
+    args["direct_string"] = "--direct" if args["direct"] else ""
+
+    cmd = (  "smurff"
+             + " --train={datadir}/{train_file}"
+             + " --test={datadir}/{test_file}" 
+             + " --prior={prior_string}"
+             + " --side-info={side_info_string}"
+             + " --aux-data={aux_data_string}"
+             + " {direct_string}"
+             + " --num-latent={num_latent} --burnin={burnin} --nsamples={nsamples}"
+             + " --precision={precision} --verbose={verbose} --status=stats.csv"
+             ).format(**args)
+
+    class Result:
+         pass
+
+    result = Result()
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        for f in [ args["train_file"], args["test_file"] ] + args["side_info_files"] + args["aux_data_files"]:
+            if f != "none":
+                os.symlink(os.path.join(args["datadir"], f), os.path.join(tmpdirname, f))
+    
+        result.retcode = call(cmd, shell=True, cwd=tmpdirname)
+        result.rmse = float("nan")
+        try:
+            stats = pd.read_csv(os.path.join(tmpdirname, "stats.csv"), sep=";")
+            last_row = stats.tail(1)
+            result.rmse = float(last_row['rmse_avg'])
+        except:
+            pass
+
+    return result
+        
 def load_data():
-    download()
+    # download()
 
     files = [
             "train.sdm",
@@ -24,145 +66,101 @@ def load_data():
 
     data = dict()
     for f in files:
-        key = os.path.splitext(f)[0]
-        data[key] = mio.read_matrix(f)
+        data[f] = mio.read_matrix(f)
 
     return data
 
-
-class Result:
-    pass
-
-def smurff_cmdline(cmd):
-    result = Result()
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        os.chdir(tmpdirname)
-        start = time()
-        result.retcode = call(cmd, shell=True)
-        stop = time()
-        result.time = stop - start
-        stats = pd.read_csv("stats.csv", sep=";")
-        last_row = stats.tail(1)
-        result.rmse = float(last_row['rmse_avg'])
-    return result
-    
-    
-def time_smurff(Ytrain, **kwargs):
-    start = time()
-    result = smurff.smurff(Ytrain, **kwargs)
-    stop = time()
-    result.time = stop - start
-    return result
-
-def smurff_bpmf(Ytrain, **kwargs):
-    kwargs["priors"] = ["normal", "normal"]
-    kwargs["side_info"] = [ None, None ]
-    kwargs["aux_data"] =  [ [], [] ]
-    return time_smurff(Ytrain, **kwargs)
-
-def smurff_macau(Ytrain, side_info, **kwargs):
-    priors = [ 'normal', 'normal' ]
-    for d in range(2):
-        if not side_info[d] is None:
-            priors[d] = 'macau'
-
-    kwargs["priors"] = priors
-    kwargs["side_info"] = side_info
-    kwargs["aux_data"] =  [ [], [] ]
-
-    return time_smurff(Ytrain, **kwargs)
-
-
 class TestExCAPE(unittest.TestCase):
-    
-    default_opts = {
-            "num_latent" : 16,
-            "burnin"     : 100,
-            "nsamples"   : 200,
-            "precision"  : 1.0,
-            "verbose"    : 0, 
-        }
+
+    def get_default_opts(self):
+        return {
+                "datadir"         : os.getcwd(),
+                "test_file"       : "test.sdm",
+                "train_file"      : "train.sdm",
+                "num_latent"      : 16,
+                "burnin"          : 100,
+                "nsamples"        : 200,
+                "precision"       : 1.0,
+                "verbose"         : 0, 
+                "aux_data_files"  : [ "none", "none" ],
+                "side_info_files" : [ "none", "none" ],
+                "direct"          : True,
+                }
 
     @classmethod
     def setUpClass(cls):
         cls.data = load_data()
+        
 
-    def test_smurff_bpmf_cmd(self):
-        params = { "datadir" : os.getcwd() }
-        result = smurff_cmdline(
-             ("smurff"
-           + " --train={datadir}/train.sdm"
-           + " --test={datadir}/test.sdm" 
-           + " --prior=normal normal"
-           + " --side-info=none none"
-           + " --aux-data=none none"
-           + " --save-freq=0"
-           + " --num-latent=16 --burnin=100 --nsamples=100"
-           + " --precision=1.0 --verbose=1 --status=stats.csv").format(**params)
-        )
+    def files_to_data(self, args):
 
-        self.assertLess(result.rmse, 1.22)
-        self.assertGreater(result.rmse, 1.10)
-        self.assertLess(result.time, 60.)
+        args["train"] = TestExCAPE.data[args["train_file"]]
+        args["Ytest"] = TestExCAPE.data[args["test_file"]]
 
-    def test_smurff_bpmf(self):
-        result = smurff_bpmf(
-                        TestExCAPE.data["train"],
-                        Ytest      = TestExCAPE.data["test"],
-                        **TestExCAPE.default_opts)
+        for t in ["side_info", "aux_data"]:
+            args[t] = []
+            for f in args[t + "_files"]:
+                if f != "none":
+                    args[t].append(TestExCAPE.data[f])
+                else:
+                    args[t].append(None)
 
-        self.assertLess(result.rmse, 1.22)
-        self.assertGreater(result.rmse, 1.10)
-        self.assertLess(result.time, 60.)
+        return args
 
-    def test_smurff_macau_c2v(self):
-        result = smurff_macau(
-                          TestExCAPE.data["train"],
-                        [ TestExCAPE.data["side_c2v"], None ],
-                        Ytest      = TestExCAPE.data["test"],
-                        **TestExCAPE.default_opts)
+    def time_smurff(self, args, expected):
+        args = self.files_to_data(args)
 
-        self.assertLess(result.rmse, 1.08)
-        self.assertGreater(result.rmse, 1.0)
-        self.assertLess(result.time, 120.)
+        for smurff in [ smurff_cmd, smurff_py ]:
+            start = time()
+            result = smurff(args)
+            stop = time()
+            elapsed = stop - start
 
-    def test_smurff_macau_ecfp_sparse_direct(self):
-        opts = TestExCAPE.default_opts
-        opts["direct"] = True
-        result = smurff_macau(
-                          TestExCAPE.data["train"],
-                        [ TestExCAPE.data["side_ecfp6_counts_var005"], None ],
-                        Ytest      = TestExCAPE.data["test"],
-                        **TestExCAPE.default_opts)
+            self.assertLess(result.rmse, expected[0])
+            self.assertGreater(result.rmse, expected[1])
+            self.assertLess(elapsed, expected[2])
 
-        self.assertLess(result.rmse, 1.17)
-        self.assertGreater(result.rmse, 1.0)
-        self.assertLess(result.time, 60.)
+    def bpmf(self, args, expected):
+        args["priors"] = ["normal", "normal"]
+        args["side_info_files"] = [ "none", "none" ]
+        self.time_smurff(args, expected)
 
-    def test_smurff_macau_ecfp_sparse_cg(self):
-        opts = TestExCAPE.default_opts
-        opts["direct"] = False
-        result = smurff_macau(
-                          TestExCAPE.data["train"],
-                        [ TestExCAPE.data["side_ecfp6_counts_var005"], None ],
-                        Ytest      = TestExCAPE.data["test"],
-                        **TestExCAPE.default_opts)
+    def macau(self, side_info_files, args, expected):
+        priors = [ 'normal', 'normal' ]
+        for d in range(2):
+            if side_info_files[d] != "none":
+                priors[d] = 'macau'
 
-        self.assertLess(result.rmse, 1.17)
-        self.assertGreater(result.rmse, 1.0)
-        self.assertLess(result.time, 360.)
+        args["priors"] = priors
+        args["side_info_files"] = side_info_files
+        args["aux_data"] =  [ [], [] ]
 
-    def test_smurff_macau_ecfp_dense(self):
-        result = smurff_macau(
-                          TestExCAPE.data["train"],
-                        [ TestExCAPE.data["side_ecfp6_folded_dense"], None ],
-                        Ytest      = TestExCAPE.data["test"],
-                        **TestExCAPE.default_opts)
+        self.time_smurff(args, expected)
 
-        self.assertLess(result.rmse, 1.08)
-        self.assertGreater(result.rmse, 1.0)
-        self.assertLess(result.time, 120.)
+    def test_bpmf(self):
+        params = self.get_default_opts()
+        self.bpmf(params, [ 1.22, 1.10, 120. ])
 
+    def test_macau_c2v(self):
+        params = self.get_default_opts()
+        side_info = [ "side_c2v.ddm", "none" ]
+        self.macau(side_info, params, [1.08, 1.0, 240. ])
+
+    def test_macau_ecfp_sparse_direct(self):
+        params = self.get_default_opts()
+        side_info = [ "side_ecfp6_counts_var005.sdm", "none" ]
+        self.macau(side_info, params, [1.17, 1.0, 120. ])
+
+    def test_macau_ecfp_sparse_cg(self):
+        params = self.get_default_opts()
+        params["direct"] = False
+        side_info = [ "side_ecfp6_counts_var005.sdm", "none" ]
+        self.macau(side_info, params, [1.17, 1.0, 480. ])
+
+    def test_macau_ecfp_dense(self):
+        params = self.get_default_opts()
+        side_info = [ "side_ecfp6_folded_dense.ddm", "none" ]
+        self.macau(side_info, params, [ 1.08, 1.0, 240. ])
 
 if __name__ == "__main__":
     unittest.main()
