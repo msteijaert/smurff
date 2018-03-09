993ab0b23ab72fdf8cf81d01e5da3f3129d48521
Tom Vander Aa <vanderaa@imec.be>
2018-03-09 14:33:14 +0100

    conda recipe for smurff-latest

--- conda-recipes/smurff-latest/build.sh
+++ conda-recipes/smurff-latest/build.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+set -x
 
-cd lib/smurff-cpp/cmake
+if [ $(uname) == "Darwin" ]
+then
+    CMAKE_OPENMP_FLAGS="-DOpenMP_CXX_FLAGS=-fopenmp=libiomp5 -DOpenMP_C_FLAGS=-fopenmp=libiomp5"
+    PYTHON_OPENMP_FLAGS="--with-openmp=libiomp5"
+else
+    CMAKE_OPENMP_FLAGS=""
+    PYTHON_OPENMP_FLAGS="--with-openmp"
+fi
+ 
+pushd lib/smurff-cpp/cmake
 rm -rf build 
 mkdir build
 cd build
-cmake ../ -DENABLE_LAPACK=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
-    -DENABLE_BOOST_RANDOM=ON \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET="" 
-make -j${CPU_COUNT} 
+cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DLAPACK_LIBRARIES=${PREFIX}/lib/libmkl_rt${SHLIB_EXT} ${CMAKE_OPENMP_FLAGS}
+make -j$CPU_COUNT
 make install
+popd
+
+pushd python/smurff
+$PYTHON setup.py install $PYTHON_OPENMP_FLAGS --single-version-externally-managed --record=record.txt
+popd
