ee74d88c36ebcc748f92325fe24f0178dbbc8896
Ilya Pasechnikov <ilya.pasechnikov@arcadia.spb.ru>
2018-03-14 14:37:23 +0300

    Fix matrix_io package unit tests for Python 2.7

--- python/matrix_io/test/test_matrix_io.py
+++ python/matrix_io/test/test_matrix_io.py
@@ -21,7 +21,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_dense_float64(self):
         matrix_filename = "test_dense_float64.ddm"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_matrix = numpy.random.randn(10, 20)
         matrix_io.write_dense_float64(matrix_relative_path, expected_matrix)
         actual_matrix = matrix_io.read_dense_float64(matrix_relative_path)
@@ -29,7 +29,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_sparse_float64(self):
         matrix_filename = "test_sparse_float64.sdm"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_matrix = scipy.sparse.rand(10, 20, 0.5)
         matrix_io.write_sparse_float64(matrix_relative_path, expected_matrix)
         actual_matrix = matrix_io.read_sparse_float64(matrix_relative_path)
@@ -37,7 +37,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_sparse_binary_matrix(self):
         matrix_filename = "test_sparse_binary_matrix.sbm"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_dense_matrix = numpy.random.randint(0, 2, size=(10, 20))
         expected_sparse_matrix = scipy.sparse.coo_matrix(expected_dense_matrix)
         matrix_io.write_sparse_binary_matrix(matrix_relative_path, expected_sparse_matrix)
@@ -46,7 +46,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_dense_float64_matrix_as_tensor(self):
         matrix_filename = "test_dense_float64_matrix_as_tensor.ddt"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_matrix = numpy.random.randn(10, 20)
         matrix_io.write_dense_float64_matrix_as_tensor(matrix_relative_path, expected_matrix)
         actual_matrix = matrix_io.read_dense_float64_tensor_as_matrix(matrix_relative_path)
@@ -54,7 +54,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_sparse_float64_matrix_as_tensor(self):
         matrix_filename = "test_sparse_float64_matrix_as_tensor.sdt"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_matrix = scipy.sparse.rand(10, 20, 0.5)
         matrix_io.write_sparse_float64_matrix_as_tensor(matrix_relative_path, expected_matrix)
         actual_matrix = matrix_io.read_sparse_float64_tensor_as_matrix(matrix_relative_path)
@@ -62,7 +62,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_csv(self):
         matrix_filename = "test_csv.csv"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_matrix = numpy.random.randn(10, 20)
         matrix_io.write_csv(matrix_relative_path, expected_matrix)
         actual_matrix = matrix_io.read_csv(matrix_relative_path)
@@ -70,7 +70,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_dense_mtx(self):
         matrix_filename = "test_dense_mtx.mtx"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_matrix = numpy.random.randn(10, 20)
         matrix_io.my_mmwrite(matrix_relative_path, expected_matrix)
         actual_matrix = scipy.io.mmread(matrix_relative_path)
@@ -78,7 +78,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_sparse_mtx(self):
         matrix_filename = "test_sparse_mtx.mtx"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_matrix = scipy.sparse.rand(10, 20, 0.5)
         matrix_io.my_mmwrite(matrix_relative_path, expected_matrix)
         actual_matrix = scipy.io.mmread(matrix_relative_path)
@@ -86,7 +86,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_matrix_ddm(self):
         matrix_filename = "test_matrix_ddt.ddm"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_matrix = numpy.random.randn(10, 20)
         matrix_io.write_matrix(matrix_relative_path, expected_matrix)
         actual_matrix = matrix_io.read_matrix(matrix_relative_path)
@@ -94,7 +94,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_matrix_sdm(self):
         matrix_filename = "test_matrix_sdm.sdm"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_matrix = scipy.sparse.rand(10, 20, 0.5)
         matrix_io.write_matrix(matrix_relative_path, expected_matrix)
         actual_matrix = matrix_io.read_matrix(matrix_relative_path)
@@ -102,7 +102,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_matrix_sbm(self):
         matrix_filename = "test_matrix_sbm.sbm"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_dense_matrix = numpy.random.randint(0, 2, size=(10, 20))
         expected_sparse_matrix = scipy.sparse.coo_matrix(expected_dense_matrix)
         matrix_io.write_matrix(matrix_relative_path, expected_sparse_matrix)
@@ -111,7 +111,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_dense_matrix_mtx(self):
         matrix_filename = "test_dense_matrix_mtx.mtx"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_matrix = numpy.random.randn(10, 20)
         matrix_io.write_matrix(matrix_relative_path, expected_matrix)
         actual_matrix = matrix_io.read_matrix(matrix_relative_path)
@@ -119,7 +119,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_matrix_sparse_mtx(self):
         matrix_filename = "test_matrix_sparse_mtx.mtx"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_matrix = scipy.sparse.rand(10, 20, 0.5)
         matrix_io.write_matrix(matrix_relative_path, expected_matrix)
         actual_matrix = matrix_io.read_matrix(matrix_relative_path)
@@ -127,7 +127,7 @@ class TestMatrixIO(unittest.TestCase):
 
     def test_dense_matrix_csv(self):
         matrix_filename = "test_dense_matrix_csv.csv"
-        matrix_relative_path = f"{self.TEMP_DIR_NAME}/{matrix_filename}"
+        matrix_relative_path = "{}/{}".format(self.TEMP_DIR_NAME, matrix_filename)
         expected_matrix = numpy.random.randn(10, 20)
         matrix_io.write_matrix(matrix_relative_path, expected_matrix)
         actual_matrix = matrix_io.read_matrix(matrix_relative_path)
