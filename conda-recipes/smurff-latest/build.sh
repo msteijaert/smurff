179091c26382d9745b5eba293dd0bcfc4d72a933
Tom Vander Aa <vanderaa@imec.be>
2018-03-16 18:27:33 +0100

    conda fixes for latests

--- conda-recipes/smurff-latest/build.sh
+++ conda-recipes/smurff-latest/build.sh
@@ -7,6 +7,8 @@ rm -rf build && mkdir build && cd build
 cmake ../lib/smurff-cpp/cmake -DCMAKE_BUILD_TYPE=Release \
     -DBoost_USE_STATIC_LIBS=OFF \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DENABLE_MKL=ON \
     -DCMAKE_C_COMPILER=$GCC \
     -DCMAKE_CXX_COMPILER=$GXX
 
