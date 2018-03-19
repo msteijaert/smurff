622024c9e66c7d1ffc0fe2b8ef28327ac31f1be4
Ilya Pasechnikov <ilya.pasechnikov@arcadia.spb.ru>
2018-03-19 15:02:22 +0300

    Fix MPIPriorFactory

--- lib/smurff-cpp/SmurffMPI/MPIPriorFactory.h
+++ lib/smurff-cpp/SmurffMPI/MPIPriorFactory.h
@@ -5,7 +5,7 @@
 #include <SmurffCpp/Priors/PriorFactory.h>
 
 namespace smurff {
-   
+
    class ILatentPrior;
    class Session;
 
@@ -13,10 +13,10 @@ namespace smurff {
    {
    public:
       template<class SideInfo>
-      std::shared_ptr<ILatentPrior> create_macau_prior(std::shared_ptr<Session> session, PriorTypes prior_type, std::shared_ptr<SideInfo> side_info,
+      std::shared_ptr<ILatentPrior> create_macau_prior(std::shared_ptr<Session> session, PriorTypes prior_type,
                                                        const std::vector<std::shared_ptr<SideInfo> >& side_infos,
                                                        const std::vector<std::shared_ptr<MacauPriorConfigItem> >& config_items);
-  
+
       std::shared_ptr<ILatentPrior> create_prior(std::shared_ptr<Session> session, int mode) override;
    };
 }
