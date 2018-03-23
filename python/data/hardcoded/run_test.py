43591986ce58733d7caa5de15f8132f9de2bb4b1
Tom Vander Aa <vanderaa@imec.be>
2018-03-23 13:23:46 +0100

    incomplete test for #91

--- python/data/hardcoded/run_test.py
+++ python/data/hardcoded/run_test.py
@@ -1,39 +1,98 @@
 #!/usr/bin/env python
 
-import smurff
-import matrix_io as mio
+import pandas as pd
 import unittest
 import os
 import tempfile
+from subprocess import call
 
 
 class TestSaveRestore(unittest.TestCase):
-    def load_data(self):
+    def defaults(self):
         files = [ "aux_0_0.mtx", "aux_1_0.mtx", "feat_0_0.mtx", "feat_1_0.mtx", "test.mtx", "train.mtx", ]
-        self.data = dict()
-        for f in files:
-            self.data[f] = mio.read_matrix(f)
+        return {
+                "datadir"         : os.getcwd(),
+                "test"            : "test.mtx",
+                "train"           : "train.mtx",
+                "save_freq"       : 1,
+                "save_prefix"       : "save",
+                "num_latent"      : 16,
+                "burnin"          : 200,
+                "verbose"         : 0, 
+                "aux_data"        : [ "none", "none" ],
+                "side_info"       : [ "none", "none" ],
+                }
 
-    def smurff(self, priors, aux_data = [[],[]], side_info = [None, None]):
-        self.load_data()
-        print(priors)
+
+    def add_datadir(self, datadir, l):
+        return [ "none" if v == "none" else os.path.join(datadir, v) for v in l ]
+
+    def make_cmd(self, args):
+        args["side_info_string"] = " ".join(self.add_datadir(args["datadir"], args["side_info"]))
+        args["aux_data_string"] = " ".join(self.add_datadir(args["datadir"], args["aux_data"]))
+        args["prior_string"] = " ".join(args["priors"])
+
+        cmd = (  "smurff"
+                + " --train={datadir}/{train}"
+                + " --test={datadir}/{test}" 
+                + " --prior={prior_string}"
+                + " --side-info={side_info_string}"
+                + " --aux-data={aux_data_string}"
+                + " --num-latent={num_latent}"
+                + " --burnin={burnin}"
+                + " --nsamples={nsamples}"
+                + " --verbose={verbose}"
+                + " --status=stats.csv"
+                + " --save-freq={save_freq}"
+                + " --save-prefix={save_prefix}"
+                + " --noise_model=\"fixed;10;1;1;1\""
+                ).format(**args)
+
+        return cmd
+
+    def run_cmd(self, pfx, cmd):
+        print(cmd)
+        # return
+        os.chdir(pfx)
+
+        rmse = float("nan")
+        retcode = call(cmd, shell=True)
+        try:
+            stats = pd.read_csv("stats.csv", sep=";")
+            last_row = stats.tail(1)
+            rmse = float(last_row['rmse_avg'])
+        except Exception as e:
+            print(e)
+
+        print(rmse)
+        return rmse
+
+    def compare(self, priors, aux_data = ["none","none"], side_info = ["none", "none"]):
+        args = self.defaults()
+        args["priors"] = priors
+        args["aux_data"] = aux_data
+        args["side_info"] = side_info
+
 
         with tempfile.TemporaryDirectory() as tmpdirname:
-             result10 = smurff.smurff(self.data["train.mtx"],
-                                 Ytest = self.data["test.mtx"],
-                                 priors = priors,
-                                 aux_data = aux_data,
-                                 side_info = side_info,
-                                 num_latent = 16,
-                                 burnin = 10,
-                                 nsamples = 10,
-                                 precision = 5.0,
-                        );
+             pfx10 = os.path.join(tmpdirname, "10")
+             pfx5 = os.path.join(tmpdirname, "5")
+
+             os.mkdir(pfx10)
+             args["nsamples"] = 100
+             cmd = self.make_cmd(args)
+             result10 = self.run_cmd(pfx10, cmd);
+
+             os.mkdir(pfx5)
+             args["nsamples"] = 50
+             cmd = self.make_cmd(args)
+             result5_1 = self.run_cmd(pfx5, cmd);
+             cmd = "sed -i -e 's/nsamples = 50/nsamples = 100/' save-options.ini; smurff --root save-root.ini"
+             result5_2 = self.run_cmd(pfx5, cmd)
         
-             print(result10.rmse)
            
     def test_normal(self):
-        self.smurff(["normal", "normal"])
+        self.compare(["normal", "normal"])
 
 if __name__ == "__main__":
     unittest.main()
