f9b029ce8e8b40b72389411608de148be45ae482
Tom Vander Aa <vanderaa@imec.be>
2018-03-30 18:26:09 +0200

    Split ini and py tests in separate files

--- python/data/excape/test_py.py
+++ python/data/excape/test_py.py
@@ -80,64 +80,6 @@ class TestExCAPE_py(unittest.TestCase):
         side_info = [ "side_ecfp6_folded_dense.ddm", None ]
         self.macau(side_info, True, [ 1.08, 1.0, 240. ])
 
-
-#
-##### run tests through ini files
-#
-
-
-
-def extract_rmse(stats_file):
-    rmse = float("nan")
-    try:
-        stats = pd.read_csv(stats_file, sep=";")
-        last_row = stats.tail(1)
-        rmse = float(last_row['rmse_avg'])
-    except Exception as e:
-        print(e)
-
-    return rmse
-
-class TestExCAPE_ini(unittest.TestCase):
-    def ini(self, ini, expected):
-            from_dir = os.getcwd()
-            link_files = [
-                    "bpmf.ini",
-                    "macau-c2v.ini",
-                    "macau-ecfp-dense.ini",
-                    "macau-ecfp-sparse-cg.ini",
-                    "macau-ecfp-sparse-direct.ini",
-                    "side_c2v.ddm",
-                    "side_ecfp6_counts_var005.sdm",
-                    "side_ecfp6_folded_dense.ddm",
-                    "test.sdm",
-                    "train.sdm",
-                ]
-            with tempfile.TemporaryDirectory() as tmpdirname:
-                for f in link_files:
-                    os.symlink(os.path.join(from_dir, f), os.path.join(tmpdirname, f))
-
-                start = time()
-                call("smurff --ini=" + ini, shell=True, cwd=tmpdirname)
-                stop = time()
-                elapsed = stop - start
-                rmse = extract_rmse(os.path.join(tmpdirname, "stats.csv"))
-
-            self.assertLess(rmse, expected[0])
-            self.assertGreater(rmse, expected[1])
-            self.assertLess(elapsed, expected[2])
-
-    def test_bpmf(self):
-        self.ini("bpmf.ini", [ 1.22, 1.10, 120. ])
-    def test_macau_c2v(self):
-        self.ini("macau-c2v.ini", [1.1, 1.0, 240. ])
-    def test_macau_ecfp_sparse_direct(self):
-        self.ini("macau-ecfp-sparse-direct.ini", [1.19, 1.0, 1500. ])
-    def test_macau_ecfp_sparse_cg(self):
-        self.ini("macau-ecfp-sparse-cg.ini", [1.19, 1.0, 900. ])
-    def test_macau_ecfp_dense(self):
-        self.ini("macau-ecfp-dense.ini", [1.08, 1.0, 240. ])
- 
 if __name__ == "__main__":
     for arg in sys.argv:
         if (arg == "-v" or arg == "--verbose"):
