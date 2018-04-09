8bd2bbd26756464336220551afa52b84b4e62513
Tom Vander Aa <vanderaa@imec.be>
2018-04-09 14:48:01 +0200

    Fix block_cg test cases

--- lib/smurff-cpp/Tests/TestsLinop.cpp
+++ lib/smurff-cpp/Tests/TestsLinop.cpp
@@ -179,7 +179,7 @@ TEST_CASE( "MatrixXd/compute_uhat", "compute_uhat for MatrixXd" ) {
    }
 }
 
-TEST_CASE( "linop/solve_blockcg_dense", "BlockCG solver for dense (3rhs separately)" ) 
+TEST_CASE( "linop/solve_blockcg_dense/fail", "BlockCG solver for dense (3rhs separately)" ) 
 {
    int rows[9] = { 0, 3, 3, 2, 5, 4, 1, 2, 4 };
    int cols[9] = { 1, 0, 2, 1, 3, 0, 1, 3, 2 };
@@ -194,11 +194,37 @@ TEST_CASE( "linop/solve_blockcg_dense", "BlockCG solver for dense (3rhs separate
         0.34,  0.05, -1.48,  1.11,
         0.09,  0.51, -0.63,  1.59;
  
-   X_true << 0.35555556,  0.40709677, -0.16444444, -0.87483871,
-             1.69333333, -0.12709677, -1.94666667,  0.49483871,
-             0.66      , -0.04064516, -0.78      ,  0.65225806;
- 
-   smurff::linop::solve_blockcg(X, sf, 0.5, B, 1e-6);
+   // this system is unsolvable
+   REQUIRE_THROWS(smurff::linop::solve_blockcg(X, sf, 0.5, B, 1e-6));
+}
+
+TEST_CASE( "linop/solve_blockcg_dense/ok", "BlockCG solver for dense (3rhs separately)" ) 
+{
+   double reg = 0.5;
+
+   Eigen::MatrixXd KK(6, 6);
+   KK <<  1.7488399 , -1.92816395, -1.39618642, -0.2769755 , -0.52815529, 0.24624319,
+        -1.92816395,  3.34435465,  2.07258617,  0.4417173 ,  0.84673143, -0.35075244,
+        -1.39618642,  2.07258617,  2.1623261 ,  0.25923918,  0.64428255, -0.2329581,
+        -0.2769755 ,  0.4417173 ,  0.25923918,  0.6147927 ,  0.15112057, -0.00692033,
+        -0.52815529,  0.84673143,  0.64428255,  0.15112057,  0.80141217, -0.19682322,
+         0.24624319, -0.35075244, -0.2329581 , -0.00692033, -0.19682322, 0.56240547;
+
+   Eigen::MatrixXd K = KK.llt().matrixU();
+
+   REQUIRE(((K.transpose() * K) - KK).norm() < 1e-3);
+
+   Eigen::MatrixXd X_true(3, 6);
+   X_true << 0.35555556,  0.40709677, -0.16444444, -0.87483871, -0.16444444, -0.87483871,
+             1.69333333, -0.12709677, -1.94666667,  0.49483871, -1.94666667,  0.49483871,
+             0.66      , -0.04064516, -0.78      ,  0.65225806, -0.78      ,  0.65225806;
+
+   Eigen::MatrixXd B = ((K.transpose() * K + Eigen::MatrixXd::Identity(6,6) * reg) * X_true.transpose()).transpose();
+   Eigen::MatrixXd X(3, 6);
+
+   //-- Solves the system (K' * K + reg * I) * X = B for X for m right-hand sides
+   smurff::linop::solve_blockcg(X, K, 0.5, B, 1e-6);
+
    for (int i = 0; i < X.rows(); i++) {
      for (int j = 0; j < X.cols(); j++) {
        REQUIRE( X(i,j) == Approx(X_true(i,j)) );
