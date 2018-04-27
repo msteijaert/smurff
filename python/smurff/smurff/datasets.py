1df2971e2025356040953984722e74b0f46af020
Tom Vander Aa <vanderaa@imec.be>
2018-04-27 13:47:15 +0200

    python load_chembl: files in tempdir

--- python/smurff/smurff/datasets.py
+++ python/smurff/smurff/datasets.py
@@ -1,5 +1,6 @@
 from .prepare import make_train_test
 
+import os
 import tempfile
 import scipy.io as sio
 from hashlib import sha256
@@ -9,29 +10,38 @@ try:
 except ImportError:
     import urllib2 as urllib_request  # for Python 2
 
-urls = [
+urls = {
+        "chembl-IC50-346targets.mm" : 
         (
             "http://homes.esat.kuleuven.be/~jsimm/chembl-IC50-346targets.mm",
             "10c3e1f989a7a415a585a175ed59eeaa33eff66272d47580374f26342cddaa88",
-            "chembl-IC50-346targets.mm",
-            ),
+        ),
+
+        "chembl-IC50-compound-feat.mm" : 
         (
             "http://homes.esat.kuleuven.be/~jsimm/chembl-IC50-compound-feat.mm",
             "f9fe0d296272ef26872409be6991200dbf4884b0cf6c96af8892abfd2b55e3bc",
-            "chembl-IC50-compound-feat.mm", 
-            ),
-        ]
-def download_chembl():
+        ),
+}
+
+def load_one(filename):
+    (url, expected_sha) =  urls["filename"]
+
     with tempfile.TemporaryDirectory() as tmpdirname:
-        for url, expected_sha, output in urls:
+            output = os.path.join(tmpdirname, filename)
             urllib_request.urlretrieve(url, output)
             actual_sha = sha256(open(output, "rb").read()).hexdigest()
             assert actual_sha == expected_sha
+            matrix = sio.mmread(output)
 
-        ic50 = sio.mmread("chembl-IC50-346targets.mm")
-        feat = sio.mmread("chembl-IC50-compound-feat.mm")
+    return matrix
 
-        ## creating train and test sets
-        ic50_train, ic50_test = make_train_test(ic50, 0.2)
+def load_chembl():
+    # load bioactivity and features
+    ic50 = load_one("chembl-IC50-346targets.mm")
+    feat = load_one("chembl-IC50-compound-feat.mm")
 
-        return (ic50_train, ic50_test, feat)
+    ## creating train and test sets
+    ic50_train, ic50_test = make_train_test(ic50, 0.2)
+
+    return (ic50_train, ic50_test, feat)
