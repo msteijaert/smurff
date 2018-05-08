c1b7dfbd9ef0ca326155b71c842eabafbaf5f8ed
Tom Vander Aa <vanderaa@imec.be>
2018-05-08 15:44:27 +0200

    Cleanup predict tests

--- python/smurff/test/test_predict.py
+++ python/smurff/test/test_predict.py
@@ -6,9 +6,9 @@ import smurff
 
 verbose = 0
 
-class TestNoiseModels(unittest.TestCase):
+class TestPredictSession(unittest.TestCase):
     # Python 2.7 @unittest.skip fix
-    __name__ = "TestNoiseModels"
+    __name__ = "TestPredictSession"
 
     def run_train_session(self):
         Y = scipy.sparse.rand(15, 10, 0.2)
@@ -28,35 +28,34 @@ class TestNoiseModels(unittest.TestCase):
 
         return session
 
-    # 5 different noise configs
-
     def test_simple(self):
         train_session = self.run_train_session()
         predict_session = train_session.makePredictSession()
 
-        p1 = train_session.getTestPredictions()
-        p2 = predict_session.predict_some(self.Ytest)
-
-        p1 = sorted(p1)
-        p2 = sorted(p2)
-
+        p1 = sorted(train_session.getTestPredictions())
+        p2 = sorted(predict_session.predict_some(self.Ytest))
         p3 = predict_session.predict_one(p1[0].coords, p1[0].val)
         p4 = predict_session.predict_all()
 
         self.assertEqual(len(p1), len(p2))
 
+        # check train_session vs predict_session for Ytest
         self.assertEqual(p1[0].coords, p2[0].coords)
         self.assertAlmostEqual(p1[0].val, p2[0].val, places = 2)
         self.assertAlmostEqual(p1[0].pred_1sample, p2[0].pred_1sample, places = 2)
         self.assertAlmostEqual(p1[0].pred_avg, p2[0].pred_avg, places = 2)
 
+        # check predict_session.predict_some vs predict_session.predict_one
         self.assertEqual(p1[0].coords, p3.coords)
         self.assertAlmostEqual(p1[0].val, p3.val, places = 2)
         self.assertAlmostEqual(p1[0].pred_1sample, p3.pred_1sample, places = 2)
         self.assertAlmostEqual(p1[0].pred_avg, p3.pred_avg, places = 2)
 
-        ecoords = (Ellipsis,) + p2[0].coords
-        [ self.assertAlmostEqual(*p, places=2) for p in zip(p2[0].pred_all, p4[ecoords]) ]
+        # check predict_session.predict_some vs predict_session.predict_all
+        for s in p2:
+            ecoords = (Ellipsis,) + s.coords
+            for p in zip(s.pred_all, p4[ecoords]):
+                self.assertAlmostEqual(*p, places=2)
 
         p1_rmse_avg = smurff.calc_rmse(p1)
         p2_rmse_avg = smurff.calc_rmse(p2)
