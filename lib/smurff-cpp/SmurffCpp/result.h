f689d3608713eb95618044dc5cbefc797b21cc85
Tom Vander Aa <vanderaa@imec.be>
2018-05-18 17:10:16 +0200

    Set ResultItems from TensorConfig

--- lib/smurff-cpp/SmurffCpp/result.h
+++ lib/smurff-cpp/SmurffCpp/result.h
@@ -84,6 +84,7 @@ private:
    void restoreState(std::shared_ptr<const StepFile> sf);
 
    std::shared_ptr<TensorConfig> toSparseTensor() const;
+   void fromSparseTensor(const std::shared_ptr<TensorConfig> &);
 
 private:
    void init();
