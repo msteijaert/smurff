5c184dadcf7aace4bff870114452979a82157e16
Tom Vander Aa <vanderaa@imec.be>
2018-09-28 20:17:41 +0200

    conda: only force intel openmp on mac os

--- conda-recipes/smurff-0.13.1/build.sh
+++ conda-recipes/smurff-0.13.1/build.sh
@@ -4,8 +4,16 @@ rm -rf build
 mkdir build
 cd build
 
+if -n "$OSX_ARCH" 
+then
+    OPENMP_FLAGS="-DOpenMP_CXX_FLAG=-fopenmp=libiomp5 -DOpenMP_C_FLAG=-fopenmp=libiomp5"
+else
+    OPENMP_FLAGS=""
+fi
+
+
 cmake ../lib/smurff-cpp/cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DENABLE_MKL=ON -DOpenMP_CXX_FLAG=-fopenmp=libiomp5 -DOpenMP_C_FLAG=-fopenmp=libiomp5
+    -DENABLE_MKL=ON $OPENMP_FLAGS
 
 make -j$CPU_COUNT
 make install
