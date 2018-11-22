2426ec5a9fcb6aa927f9784705a0c6982955c1cd
Tom Vander Aa <vanderaa@imec.be>
2018-11-22 15:17:04 +0100

    ENH: viennacl tests

---
+++ lib/smurff-cpp/Tests/TestsViennaCL.cpp
@@ -0,0 +1,48 @@
+#include "catch.hpp"
+
+#include "viennacl/matrix.hpp"
+
+#include <Eigen/Core>
+
+TEST_CASE("RowMajor/ColMajor", "[viennacl]")
+{
+   #define SHOW(M) std::cout << #M << " =\n" << M << std::endl << std::endl;
+
+   typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> EigenRowmajor;
+   EigenRowmajor eigen_rowmajor(3, 2);
+   eigen_rowmajor << 1, 2, 3, 4, 5, 6;
+   SHOW(eigen_rowmajor);
+
+   typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> EigenColmajor;
+   EigenColmajor eigen_colmajor(3, 2);
+   eigen_colmajor << 1, 2, 3, 4, 5, 6;
+   SHOW(eigen_colmajor);
+
+   Eigen::Map<EigenColmajor> eigen_implicit_transpose_rowmajor(eigen_rowmajor.data(), 2, 3);
+   SHOW(eigen_implicit_transpose_rowmajor);
+
+   Eigen::Map<EigenRowmajor> eigen_implicit_transpose_colmajor(eigen_colmajor.data(), 2, 3);
+   SHOW(eigen_implicit_transpose_colmajor);
+
+   using namespace viennacl;
+
+   matrix<double, row_major> vcl_rowmajor_from_eigen_rowmajor(3, 2);
+   copy(eigen_rowmajor, vcl_rowmajor_from_eigen_rowmajor);
+   SHOW(vcl_rowmajor_from_eigen_rowmajor);
+
+   matrix<double, row_major> vcl_rowmajor_from_eigen_colmajor(3, 2);
+   copy(eigen_colmajor, vcl_rowmajor_from_eigen_colmajor);
+   SHOW(vcl_rowmajor_from_eigen_colmajor);
+
+   matrix<double, column_major> vcl_colmajor_from_eigen_rowmajor(3, 2);
+   copy(eigen_rowmajor, vcl_colmajor_from_eigen_rowmajor);
+   SHOW(vcl_colmajor_from_eigen_rowmajor);
+
+   matrix<double, column_major> vcl_colmajor_from_eigen_colmajor(3, 2);
+   copy(eigen_colmajor, vcl_colmajor_from_eigen_colmajor);
+   SHOW(vcl_colmajor_from_eigen_colmajor);
+
+   matrix<double, row_major> vcl_rowmajor_from_eigen_colmajor_transposed(2, 3);
+   copy(eigen_implicit_transpose_colmajor, vcl_rowmajor_from_eigen_colmajor_transposed);
+   SHOW(vcl_rowmajor_from_eigen_colmajor_transposed);
+}
