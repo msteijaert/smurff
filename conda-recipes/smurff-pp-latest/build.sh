d49c6d68488bf9cf93b83376ef4d50208058d541
Tom Vander Aa <vanderaa@imec.be>
2019-11-14 10:17:48 +0100

    WIP: conda recipe for smurff-pp git

---
+++ conda-recipes/smurff-pp-latest/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pushd lib/smurff-cpp/cmake
+rm -rf build 
+mkdir build
+cd build
+
+if [ -n "$OSX_ARCH" ]
+then
+    OPENMP_FLAGS="-DOpenMP_CXX_FLAG=-fopenmp=libiomp5 -DOpenMP_C_FLAG=-fopenmp=libiomp5"
+else
+    OPENMP_FLAGS=""
+fi
+
+
+cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DENABLE_MKL=ON $OPENMP_FLAGS
+
+make -j$CPU_COUNT
+make install
+
+cd python/Smurff
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+popd
